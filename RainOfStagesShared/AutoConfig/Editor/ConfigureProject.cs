608c06e9a605b3d19da5777757414b0987a2d6e3
James LaPenn <james.lapenn@gmail.com>
2020-05-03 14:45:56 -0400

    Setup project automatic configuration on load

--- RainOfStagesShared/AutoConfig/Editor/ConfigureProject.cs
+++ RainOfStagesShared/AutoConfig/Editor/ConfigureProject.cs
@@ -1,144 +1,169 @@
-﻿//using System;
-//using System.IO;
-//using System.Linq;
-//using UnityEditor;
-//using UnityEngine;
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
 
-//namespace RainOfStages.AutoConfig
-//{
-//    [InitializeOnLoad]
-//    public class ConfigureProject
-//    {
-//        private const string RoR2Executable = "Risk of Rain 2.exe";
+namespace RainOfStages.AutoConfig
+{
+    [InitializeOnLoad]
+    public class ConfigureProject
+    {
 
-//        static ConfigureProject() => EditorApplication.update += OnUpdate;
+        private static string[] RequiredAssemblies = new[]
+        {
+            "Assembly-CSharp",
+            "Facepunch.Steamworks",
+            "Rewired_Core",
+            "Rewired_CSharp",
+            "Rewired_Windows_Lib",
+            "Unity.Postprocessing.Runtime",
+            "WWise",
+            "Zio"
+        };
+        private const string RoR2Executable = "Risk of Rain 2.exe";
 
-//        private static void OnUpdate()
-//        {
-//            const string AsmCSharp = "Assembly-CSharp.dll";
-//            var settings = RainOfStagesSettings.GetOrCreateSettings();
-//            var results = AssetDatabase.FindAssets("Assembly-CSharp");
+        static ConfigureProject()
+        {
+            EditorApplication.update += ValidateRoRReferences;
+            AssemblyReloadEvents.beforeAssemblyReload += ValidateRoRReferences;
+        }
 
-//            if (results.Any() && !string.IsNullOrEmpty(settings.RoR2Path) && File.Exists(Path.Combine(settings.RoR2Path, "Risk of Rain 2.exe")))
-//                return;
+        private static void ValidateRoRReferences()
+        {
+            string projectDirectory = Directory.GetCurrentDirectory();
+            var settings = RainOfStagesSettings.GetOrCreateSettings();
+            var results = RequiredAssemblies.SelectMany(ra => AssetDatabase.FindAssets(ra)).Distinct().ToArray();
+            var destinationFolder = Path.Combine(projectDirectory, "Assets", "Assemblies");
 
-//            Debug.Log("Acquiring path for Rain of Stages");
+            if (RequiredAssemblies.All(asm => File.Exists(Path.Combine(destinationFolder, asm))))
+                return;
 
-//            string projectDirectory = Directory.GetCurrentDirectory();
-//            string ror2Path = projectDirectory;
-//            while (!Directory.EnumerateFiles(ror2Path, RoR2Executable).Any())
-//                ror2Path = EditorUtility.OpenFolderPanel("Open Risk of Rain 2 Root Folder", Directory.GetCurrentDirectory(), RoR2Executable);
+            if (results.Length == RequiredAssemblies.Length 
+             && !string.IsNullOrEmpty(settings.RoR2Path)
+             && File.Exists(Path.Combine(settings.RoR2Path, "Risk of Rain 2.exe")))
+                return;
 
-//            settings.RoR2Path = ror2Path;
-//            settings.SetDirty();
+            Debug.Log("Acquiring references for Rain of Stages");
 
-//            var assemblyPath = Path.Combine(ror2Path, "Risk of Rain 2_Data", "Managed", AsmCSharp);
-//            var destination = Path.Combine(projectDirectory, "Assets", "Assemblies", AsmCSharp);
-//            var destinationMetaData = Path.Combine(projectDirectory, "Assets", "Assemblies", $"{AsmCSharp}.meta");
+            string ror2Path = projectDirectory;
+            while (!Directory.EnumerateFiles(ror2Path, RoR2Executable).Any())
+                ror2Path = EditorUtility.OpenFolderPanel("Open Risk of Rain 2 Root Folder", Directory.GetCurrentDirectory(), RoR2Executable);
+
+            settings.RoR2Path = ror2Path;
+            settings.SetDirty();
+
+            foreach (var asm in RequiredAssemblies)
+            {
+                var destinationFile = Path.Combine(projectDirectory, "Assets", "Assemblies", $"{asm}.dll");
+                var assemblyPath = Path.Combine(ror2Path, "Risk of Rain 2_Data", "Managed", $"{asm}.dll");
+                var destinationMetaData = Path.Combine(projectDirectory, "Assets", "Assemblies", $"{asm}.meta");
+
+                if (File.Exists(destinationFile)) File.Delete(destinationFile);
+                File.Copy(assemblyPath, destinationFile);
+
+                File.WriteAllText(destinationMetaData, MetaData);
+            }
+
+            AssetDatabase.Refresh();
+        }
 
 
-//            if (File.Exists(destination)) File.Delete(destination);
-//            File.Copy(assemblyPath, destination);
-
-//            File.WriteAllText(destinationMetaData, MetaData);
-//            AssetDatabase.Refresh();
-//        }
-
-//        const string MetaData =
-//@"fileFormatVersion: 2
-//guid: fc64261ca6282254da01b0f016bcfcea
-//PluginImporter:
-//  externalObjects: {}
-//  serializedVersion: 2
-//  iconMap: {}
-//  executionOrder: {}
-//  defineConstraints: []
-//  isPreloaded: 0
-//  isOverridable: 0
-//  isExplicitlyReferenced: 1
-//  validateReferences: 0
-//  platformData:
-//  - first:
-//      '': Any
-//    second:
-//      enabled: 0
-//      settings:
-//        Exclude Editor: 0
-//        Exclude Linux: 0
-//        Exclude Linux64: 0
-//        Exclude LinuxUniversal: 0
-//        Exclude OSXUniversal: 0
-//        Exclude Win: 0
-//        Exclude Win64: 0
-//  - first:
-//      Any: 
-//    second:
-//      enabled: 1
-//      settings: {}
-//  - first:
-//      Editor: Editor
-//    second:
-//      enabled: 1
-//      settings:
-//        CPU: AnyCPU
-//        DefaultValueInitialized: true
-//        OS: AnyOS
-//  - first:
-//      Facebook: Win
-//    second:
-//      enabled: 0
-//      settings:
-//        CPU: AnyCPU
-//  - first:
-//      Facebook: Win64
-//    second:
-//      enabled: 0
-//      settings:
-//        CPU: AnyCPU
-//  - first:
-//      Standalone: Linux
-//    second:
-//      enabled: 1
-//      settings:
-//        CPU: x86
-//  - first:
-//      Standalone: Linux64
-//    second:
-//      enabled: 1
-//      settings:
-//        CPU: x86_64
-//  - first:
-//      Standalone: LinuxUniversal
-//    second:
-//      enabled: 1
-//      settings: {}
-//  - first:
-//      Standalone: OSXUniversal
-//    second:
-//      enabled: 1
-//      settings:
-//        CPU: AnyCPU
-//  - first:
-//      Standalone: Win
-//    second:
-//      enabled: 1
-//      settings:
-//        CPU: AnyCPU
-//  - first:
-//      Standalone: Win64
-//    second:
-//      enabled: 1
-//      settings:
-//        CPU: AnyCPU
-//  - first:
-//      Windows Store Apps: WindowsStoreApps
-//    second:
-//      enabled: 0
-//      settings:
-//        CPU: AnyCPU
-//  userData: 
-//  assetBundleName: 
-//  assetBundleVariant: 
-//";
-//    }
-//}
+        const string MetaData =
+@"fileFormatVersion: 2
+guid: fc64261ca6282254da01b0f016bcfcea
+PluginImporter:
+  externalObjects: {}
+  serializedVersion: 2
+  iconMap: {}
+  executionOrder: {}
+  defineConstraints: []
+  isPreloaded: 0
+  isOverridable: 0
+  isExplicitlyReferenced: 0
+  validateReferences: 1
+  platformData:
+  - first:
+      '': Any
+    second:
+      enabled: 0
+      settings:
+        Exclude Editor: 0
+        Exclude Linux: 0
+        Exclude Linux64: 0
+        Exclude LinuxUniversal: 0
+        Exclude OSXUniversal: 0
+        Exclude Win: 0
+        Exclude Win64: 0
+  - first:
+      Any: 
+    second:
+      enabled: 1
+      settings: {}
+  - first:
+      Editor: Editor
+    second:
+      enabled: 1
+      settings:
+        CPU: AnyCPU
+        DefaultValueInitialized: true
+        OS: AnyOS
+  - first:
+      Facebook: Win
+    second:
+      enabled: 0
+      settings:
+        CPU: AnyCPU
+  - first:
+      Facebook: Win64
+    second:
+      enabled: 0
+      settings:
+        CPU: AnyCPU
+  - first:
+      Standalone: Linux
+    second:
+      enabled: 1
+      settings:
+        CPU: x86
+  - first:
+      Standalone: Linux64
+    second:
+      enabled: 1
+      settings:
+        CPU: x86_64
+  - first:
+      Standalone: LinuxUniversal
+    second:
+      enabled: 1
+      settings: {}
+  - first:
+      Standalone: OSXUniversal
+    second:
+      enabled: 1
+      settings:
+        CPU: AnyCPU
+  - first:
+      Standalone: Win
+    second:
+      enabled: 1
+      settings:
+        CPU: AnyCPU
+  - first:
+      Standalone: Win64
+    second:
+      enabled: 1
+      settings:
+        CPU: AnyCPU
+  - first:
+      Windows Store Apps: WindowsStoreApps
+    second:
+      enabled: 0
+      settings:
+        CPU: AnyCPU
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 
+";
+    }
+}
