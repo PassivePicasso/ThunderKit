55c4bf627d2e6d307cc50b53b8a52b4c5d7f497f
James LaPenn <james.lapenn@gmail.com>
2020-05-03 23:26:14 -0400

    An Organized project is a happy project. Add Thunderstore Manifest object. Create and Dispose WebClient per use instead of storing unnecessarily.

---
+++ RainOfStages/Thunderstore/Manifest.cs
@@ -0,0 +1,22 @@
+﻿using RainOfStages.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace RainOfStages.Thunderstore
+{
+    public class Manifest : ScriptableObject
+    {
+        public string version_number;
+        public string website_url;
+        public string description;
+        public string[] dependencies;
+
+        [MenuItem("Assets/Rain of Stages/Mod Manifest")]
+        public static void Create()
+        {
+            ScriptableHelper.CreateAsset<Manifest>("Manifest");
+        }
+    }
+}
