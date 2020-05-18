0d9568dd7be5c0ef9bf1d66ff7609d6966631154
James LaPenn <james.lapenn@gmail.com>
2020-05-17 23:27:29 -0400

    Greatly simplify new stage setup

--- RainOfStages/RoR2/Editor/CreateAsset.cs
+++ RainOfStages/RoR2/Editor/CreateAsset.cs
@@ -1,11 +1,18 @@
 ﻿using RainOfStages.Proxy;
 using RainOfStages.Utilities;
 using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace RainOfStages.Editor
 {
-    public class CreateAsset
+    public class CreateAsset : ScriptableObject
     {
+        public GameObject Director;
+        public GameObject GlobalEventManager;
+        public GameObject SceneInfo;
+
         [MenuItem("Assets/Rain of Stages/" + nameof(SurfaceDef))]
         public static void CreateSurfaceDef() => ScriptableHelper.CreateAsset<SurfaceDef>();
 
@@ -29,7 +36,52 @@ namespace RainOfStages.Editor
         [MenuItem("Assets/Rain of Stages/Stages/" + nameof(SceneDefinition))]
         public static void CreateCustomSceneProxy() => ScriptableHelper.CreateAsset<SceneDefinition>();
 
+        [MenuItem("Assets/Rain of Stages/Stages/New Stage")]
+        public static void CreateStage()
+        {
+            var createAsset = ScriptableObject.CreateInstance<CreateAsset>();
+
+
+            Scene scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+            PrefabUtility.InstantiatePrefab(createAsset.Director, scene);
+            PrefabUtility.InstantiatePrefab(createAsset.GlobalEventManager, scene);
+            PrefabUtility.InstantiatePrefab(createAsset.SceneInfo, scene);
+            var worldObject = new GameObject("World");
+            worldObject.layer = LayerMask.NameToLayer("World");
+
+            var lightObject = new GameObject("Directional Light (SUN)", typeof(Light));
+            lightObject.transform.forward = Vector3.forward + Vector3.down + Vector3.right;
+
+            var light = lightObject.GetComponent<Light>();
+            light.type = LightType.Directional;
+            light.intensity = 1.2f;
+            (float h, float s, float v) color = (0, 0, 0);
+            Color.RGBToHSV(Color.cyan, out color.h, out color.s, out color.v);
+            color.s = 0.5f;
+            color.v = 0.8f;
+            light.lightmapBakeType = LightmapBakeType.Realtime;
+            light.color = Color.HSVToRGB(color.h, color.s, color.v);
+            RenderSettings.sun = light;
+            RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Flat;
+            RenderSettings.ambientSkyColor = light.color;
+            RenderSettings.ambientGroundColor = light.color;
+            RenderSettings.ambientEquatorColor = light.color;
+            RenderSettings.ambientIntensity = 0.8f;
+            Lightmapping.realtimeGI = false;
+            Lightmapping.giWorkflowMode = Lightmapping.GIWorkflowMode.OnDemand;
+            Lightmapping.bakedGI = false;
+            DynamicGI.UpdateEnvironment();
+
+            //var tab = new LightingExplorerTab()
+
+        }
+
+
         [MenuItem("Assets/Rain of Stages/Modding Assets/" + nameof(BakeSettings))]
-        public static void Create() => ScriptableHelper.CreateAsset<BakeSettings>();
+        public static void CreateBakeSettings() => ScriptableHelper.CreateAsset<BakeSettings>();
+
+
+
+
     }
 }
