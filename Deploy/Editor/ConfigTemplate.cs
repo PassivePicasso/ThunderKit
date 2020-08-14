84bd30511e58a2e4f03f47fe40d422a0342d8e14
James LaPenn <james.lapenn@gmail.com>
2020-08-14 14:20:17 -0400

    fix log levels implementation

--- Deploy/Editor/ConfigTemplate.cs
+++ Deploy/Editor/ConfigTemplate.cs
@@ -6,77 +6,121 @@
             string.Format(Content, consoleEnabled.ToString().ToLower(), logLevel);
 
         public static readonly string Content = @"[Caching]
+
 ## Enable/disable assembly metadata cache
 ## Enabling this will speed up discovery of plugins and patchers by caching the metadata of all types BepInEx discovers.
 # Setting type: Boolean
-# Default value: True
+# Default value: true
 EnableAssemblyCache = true
+
+[Harmony.Logger]
+
+## Specifies which Harmony log channels to listen to.
+## NOTE: IL channel dumps the whole patch methods, use only when needed!
+# Setting type: LogChannel
+# Default value: Warn, Error
+# Acceptable values: None, Info, IL, Warn, Error, All
+# Multiple values can be set at the same time by separating them with , (e.g. Debug, Warning)
+LogChannels = Warn, Error
 
 [Logging]
 
-## Redirects text from Console.Out during preloader patch loading to the BepInEx logging system.
+## Enables showing unity log messages in the BepInEx logging system.
 # Setting type: Boolean
-# Default value: True
-PreloaderConsoleOutRedirection = true
-
+# Default value: true
 UnityLogListening = true
+
+## If enabled, writes Standard Output messages to Unity log
+## NOTE: By default, Unity does so automatically. Only use this option if no console messages are visible in Unity log
+## 
+# Setting type: Boolean
+# Default value: false
+LogConsoleToUnityLog = false
+
+PreloaderConsoleOutRedirection = true
 
 [Logging.Console]
 
 ## Enables showing a console for log output.
 # Setting type: Boolean
-# Default value: False
-Enabled = {0}
+# Default value: false
+Enabled = true
 
 ## If true, console is set to the Shift-JIS encoding, otherwise UTF-8 encoding.
 # Setting type: Boolean
-# Default value: False
+# Default value: false
 ShiftJisEncoding = false
 
-DisplayedLogLevel = {1}
+## Which log levels to show in the console output.
+# Setting type: LogLevel
+# Default value: Fatal, Error, Warning, Message, Info
+# Acceptable values: None, Fatal, Error, Warning, Message, Info, Debug, All
+# Multiple values can be set at the same time by separating them with , (e.g. Debug, Warning)
+LogLevels = Fatal, Error, Warning, Message, Info
+
+DisplayedLogLevel = Debug
 
 [Logging.Disk]
 
+## Include unity log messages in log file output.
+# Setting type: Boolean
+# Default value: false
 WriteUnityLog = true
 
+## Appends to the log file instead of overwriting, on game startup.
+# Setting type: Boolean
+# Default value: false
 AppendLog = false
 
-Enabled = true
+## Enables writing log messages to disk.
+# Setting type: Boolean
+# Default value: true
+Enabled = {0}
 
-DisplayedLogLevel = {1}
+## Which log leves are saved to the disk log output.
+# Setting type: LogLevel
+# Default value: Fatal, Error, Warning, Message, Info
+# Acceptable values: None, Fatal, Error, Warning, Message, Info, Debug, All
+# Multiple values can be set at the same time by separating them with , (e.g. Debug, Warning)
+LogLevels = {1}
+
+DisplayedLogLevel = Debug
 
 [Preloader]
 
 ## Enables or disables runtime patches.
 ## This should always be true, unless you cannot start the game due to a Harmony related issue (such as running .NET Standard runtime) or you know what you're doing.
 # Setting type: Boolean
-# Default value: True
+# Default value: true
 ApplyRuntimePatches = true
 
-## If enabled, basic Harmony functionality is patched to use MonoMod's RuntimeDetour instead.
-## Try using this if Harmony does not work in a game.
-# Setting type: Boolean
-# Default value: True
-ShimHarmonySupport = true
+## Specifies which MonoMod backend to use for Harmony patches. Auto uses the best available backend.
+## This setting should only be used for development purposes (e.g. debugging in dnSpy). Other code might override this setting.
+# Setting type: MonoModBackend
+# Default value: auto
+# Acceptable values: auto, dynamicmethod, methodbuilder, cecil
+HarmonyBackend = auto
 
 ## If enabled, BepInEx will save patched assemblies into BepInEx/DumpedAssemblies.
 ## This can be used by developers to inspect and debug preloader patchers.
 # Setting type: Boolean
-# Default value: False
+# Default value: false
 DumpAssemblies = false
 
 ## If enabled, BepInEx will load patched assemblies from BepInEx/DumpedAssemblies instead of memory.
 ## This can be used to be able to load patched assemblies into debuggers like dnSpy.
 ## If set to true, will override DumpAssemblies.
 # Setting type: Boolean
-# Default value: False
+# Default value: false
 LoadDumpedAssemblies = false
 
 ## If enabled, BepInEx will call Debugger.Break() once before loading patched assemblies.
 ## This can be used with debuggers like dnSpy to install breakpoints into patched assemblies before they are loaded.
 # Setting type: Boolean
-# Default value: False
+# Default value: false
 BreakBeforeLoadAssemblies = false
+
+ShimHarmonySupport = true
 
 [Preloader.Entrypoint]
 
