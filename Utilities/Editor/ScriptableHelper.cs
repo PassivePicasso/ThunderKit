699b2ae41f988dce9a5d9f92202f8dd5c3c1c7a3
James LaPenn <james.lapenn@gmail.com>
2020-08-30 19:49:06 -0400

    don't compile ScriptableHelper on deployment

--- Utilities/Editor/ScriptableHelper.cs
+++ Utilities/Editor/ScriptableHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿#if UNITY_EDITOR
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -31,3 +32,4 @@ namespace PassivePicasso.ThunderKit.Utilities
         }
     }
 }
+#endif
