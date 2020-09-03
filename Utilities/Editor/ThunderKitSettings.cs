b4c3bc7c8fbadb765b8abd86c1ec4dfa99954931
James LaPenn <james.lapenn@gmail.com>
2020-09-02 21:31:36 -0400

    add extra command line argument support

--- Utilities/Editor/ThunderKitSettings.cs
+++ Utilities/Editor/ThunderKitSettings.cs
@@ -28,6 +28,9 @@ namespace PassivePicasso.ThunderKit.Utilities
         public string[] deployment_exclusions;
 
         [SerializeField]
+        public string[] extraCommandLineArgs;
+
+        [SerializeField]
         public bool Is64Bit;
 
         public static ThunderKitSettings GetOrCreateSettings()
