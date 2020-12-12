f4214c51e8f462637190ea1e86b8c1297b396c16
James LaPenn <james.lapenn@gmail.com>
2020-12-12 13:12:49 -0500

    Add BundleAsset as method to decompress assetbundles for reuse

---
+++ Utilities/BundleAsset.cs
@@ -0,0 +1,54 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace PassivePicasso.ThunderKit.Utilities
+{
+    using static ScriptableHelper;
+    public class BundleAsset : ScriptableObject
+    {
+
+        [MenuItem(ThunderKitContextRoot + nameof(BundleAsset), true)]
+        public static bool CanCreate() => ".manifest".Equals(Path.GetExtension(AssetDatabase.GetAssetPath(Selection.activeObject)));
+
+        [MenuItem(ThunderKitContextRoot + nameof(BundleAsset), false)]
+        public static void Create()
+        {
+            CreateAsset<BundleAsset>(() => Selection.activeObject.name);
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            var directoryPath = Path.GetDirectoryName(path);
+            var outputDirName = $"{Selection.activeObject.name}-Assets";
+            var bundleAssetDir = Path.Combine(directoryPath, outputDirName);
+            var bundlePath = Path.Combine(directoryPath, Path.GetFileNameWithoutExtension(path));
+            if (!AssetDatabase.IsValidFolder(bundleAssetDir)) AssetDatabase.CreateFolder(directoryPath, outputDirName);
+
+            var bundle = AssetBundle.LoadFromFile(bundlePath);
+            var contents = bundle.LoadAllAssets();
+            foreach (var content in contents)
+            {
+                var duplicate = Object.Instantiate(content);
+                duplicate.name = content.name;
+
+                var assetTypeDir = Path.Combine(bundleAssetDir, content.GetType().Name);
+                if (!AssetDatabase.IsValidFolder(assetTypeDir)) AssetDatabase.CreateFolder(bundleAssetDir, content.GetType().Name);
+                try
+                {
+                    switch (duplicate)
+                    {
+                        case GameObject contentObject:
+                            PrefabUtility.SaveAsPrefabAsset(contentObject, Path.Combine(assetTypeDir, $"{content.name}.prefab"));
+                            break;
+                        default:
+                            AssetDatabase.CreateAsset(duplicate, Path.Combine(assetTypeDir, $"{content.name}.asset"));
+                            break;
+                    }
+                }
+                catch { }
+            }
+            bundle.Unload(true);
+            
+        }
+    }
+}
+#endif
