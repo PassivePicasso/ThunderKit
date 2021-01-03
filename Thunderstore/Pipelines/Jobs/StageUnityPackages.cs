23af368a9f5df49121b0d1734b492162506560f5
James LaPenn <james.lapenn@gmail.com>
2021-01-03 18:30:03 -0500

    Stop pushing some outputs to Packages as it does not work reliably

--- Thunderstore/Pipelines/Jobs/StageUnityPackages.cs
+++ Thunderstore/Pipelines/Jobs/StageUnityPackages.cs
@@ -3,7 +3,6 @@ using PassivePicasso.ThunderKit.Data;
 using PassivePicasso.ThunderKit.Pipelines;
 using System.IO;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
 
 namespace PassivePicasso.ThunderKit.Thunderstore.Pipelines.Steps
@@ -11,31 +10,21 @@ namespace PassivePicasso.ThunderKit.Thunderstore.Pipelines.Steps
     [PipelineSupport(typeof(ManifestPipeline)), ManifestProcessor]
     public class StageUnityPackages : PipelineJob
     {
+        [Tooltip("Output path Relative to the Manifest Staging root. \r\nUse %Manifest to add the Manifest name")]
+        public string outputPath;
         public override void Execute(Pipeline pipeline)
         {
-            var manifest = (pipeline as ManifestPipeline).Manifest;
+            var manifestPipeline = pipeline as ManifestPipeline;
+            var manifest = manifestPipeline.Manifest;
             if (manifest.unityPackages?.Any() != true) return;
-            var packageJsonPath = Path.Combine("Packages", nameof(StageUnityPackages), "package.json");
+
+            string resolvedOutputPath = outputPath.Replace("%Manifest/", $"{manifest.name}/").Replace("%Manifest\\", $"{manifest.name}\\");
+            var outputRoot = Path.Combine(manifestPipeline.ManifestPath, resolvedOutputPath);
             
-            var outputRoot = Path.Combine("Packages", nameof(StageUnityPackages), manifest.name);
             if (!Directory.Exists(outputRoot)) Directory.CreateDirectory(outputRoot);
 
-            if (!File.Exists(packageJsonPath))
-            {
-                File.WriteAllText(packageJsonPath, JsonUtility.ToJson(new PackageManagerManifest(
-                      "thunderkit.unitypackages",
-                      $"{nameof(StageUnityPackages)} Output",
-                      "1.0.0",
-                      Application.unityVersion.Substring(0, Application.unityVersion.LastIndexOf(".")),
-                      $"Output from {nameof(StageUnityPackages)}"
-                  )));
-
-            }
             foreach (var redistributable in manifest.unityPackages)
                 UnityPackage.Export(redistributable, outputRoot);
-
-            AssetDatabase.Refresh();
-            AssetDatabase.ImportAsset(outputRoot);
         }
     }
 }
