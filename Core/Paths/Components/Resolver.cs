900f0b586b2bf5f82a1bfbab47b535d91033cbc2
James LaPenn <james.lapenn@gmail.com>
2021-01-24 22:57:02 -0500

    Revert "Attempt Automatic Updates of string references to PathReference objects"
    This reverts commit 1289fa02070cf3c28f57025d0b50c19b018eb5c7.

--- Core/Paths/Components/Resolver.cs
+++ Core/Paths/Components/Resolver.cs
@@ -1,11 +1,9 @@
-using ThunderKit.Core.Attributes;
 using ThunderKit.Core.Pipelines;
 
 namespace ThunderKit.Core.Paths.Components
 {
     public class Resolver : PathComponent
     {
-        [PathReferenceResolver]
         public string value;
         public override string GetPath(PathReference output, Pipeline pipeline) => value.Resolve(pipeline, this);
     }
