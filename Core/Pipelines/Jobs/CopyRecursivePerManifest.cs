d34adb8fa1120b397bd02d2a1611fbfcb5ea523c
James LaPenn <james.lapenn@gmail.com>
2021-01-26 23:30:58 -0500

    Actually execute in perManifest version of CopyRecursivePerManifest

--- Core/Pipelines/Jobs/CopyRecursivePerManifest.cs
+++ Core/Pipelines/Jobs/CopyRecursivePerManifest.cs
@@ -5,8 +5,5 @@ namespace ThunderKit.Core.Pipelines.Jobs
     [PipelineSupport(typeof(Pipeline)), ManifestProcessor]
     public class CopyRecursivePerManifest : CopyRecursive
     {
-        public override void Execute(Pipeline pipeline)
-        {
-        }
     }
 }
