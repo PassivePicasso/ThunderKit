8daaf35e9356b7fa265a84676679f234d33b37b9
James LaPenn <james.lapenn@gmail.com>
2021-01-26 22:54:46 -0500

    Fix local dependency drag and drop support

--- Core/Editor/ScriptEditorHelper.cs
+++ Core/Editor/ScriptEditorHelper.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
 
@@ -11,33 +10,8 @@ namespace ThunderKit.Core.Editor
         {
             var script = MonoScript.FromScriptableObject(scriptableObject);
             var scriptPath = AssetDatabase.GetAssetPath(script);
-            var editorType = ScriptEditorUtility.GetScriptEditorFromPreferences();
-            var editorPath = ScriptEditorUtility.GetExternalScriptEditor();
-            var args = ScriptEditorUtility.GetExternalScriptEditorArgs();
-            switch (editorType)
-            {
-                case ScriptEditorUtility.ScriptEditor.SystemDefault:
-                case ScriptEditorUtility.ScriptEditor.MonoDevelop:
-                case ScriptEditorUtility.ScriptEditor.VisualStudioExpress:
-                case ScriptEditorUtility.ScriptEditor.Rider:
-                    UnityEngine.Debug.LogError($"Code Editor: {editorType} not supported by ComposableObject Edit Script command");
-                    break;
-                case ScriptEditorUtility.ScriptEditor.VisualStudio:
-                    Process.Start(new ProcessStartInfo
-                    {
-                        FileName = editorPath,
-                        Arguments = $"{args} /Edit {scriptPath}"
-                    });
-                    break;
-                case ScriptEditorUtility.ScriptEditor.Other:
-                case ScriptEditorUtility.ScriptEditor.VisualStudioCode:
-                    Process.Start(new ProcessStartInfo
-                    {
-                        FileName = editorPath,
-                        Arguments = $"{args} {scriptPath}"
-                    });
-                    break;
-            }
+
+            InternalEditorUtility.OpenFileAtLineExternal(scriptPath, -1);
 
         }
     }
