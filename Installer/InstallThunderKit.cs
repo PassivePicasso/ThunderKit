84236007a253c937ef01a2fbcfe9b63fa7b927a5
James LaPenn <james.lapenn@gmail.com>
2021-01-27 18:04:25 -0500

    create automatic installer

---
+++ Installer/InstallThunderKit.cs
@@ -0,0 +1,20 @@
+﻿#if !THUNDERKIT_CONFIGURED
+#if !IsThunderKitProject
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace PassivePicasso.RainOfStages.Installer
+{
+    [InitializeOnLoad]
+    public class InstallThunderKit
+    {
+        static InstallThunderKit()
+        {
+            Client.Add("https://github.com/PassivePicasso/ThunderKit.git");
+        }
+    }
+}
+#endif
+#endif
