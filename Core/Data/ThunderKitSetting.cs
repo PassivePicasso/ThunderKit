4bc91fac41701bb23969f48a7915e60e88378565
PassivePicasso <james.lapenn@gmail.com>
2021-02-04 22:34:54 -0500

    Style ThunderKit settings page

--- Core/Data/ThunderKitSetting.cs
+++ Core/Data/ThunderKitSetting.cs
@@ -13,12 +13,24 @@ namespace ThunderKit.Core.Data
         public static T GetOrCreateSettings<T>() where T : ThunderKitSetting =>
             ScriptableHelper.EnsureAsset<T>($"Assets/ThunderKitSettings/{typeof(T).Name}.asset", settings => settings.Initialize());
 
-        public static SerializedObject GetSerializedSettings<T>() where T : ThunderKitSetting => new SerializedObject(GetOrCreateSettings<T>());
         public virtual void Initialize() { }
         public virtual IEnumerable<string> Keywords() => Enumerable.Empty<string>();
         public virtual void CreateSettingsUI(VisualElement rootElement) { }
 
         protected static Type[] createSettingsUiParameterTypes = new[] { typeof(VisualElement) };
+
+        protected static VisualElement CreateStandardField(string gamePath)
+        {
+            var container = new VisualElement();
+            var label = new Label(ObjectNames.NicifyVariableName(gamePath));
+            var field = new TextField { bindingPath = gamePath, };
+            container.Add(label);
+            container.Add(field);
+            container.AddToClassList("thunderkit-field");
+            field.AddToClassList("thunderkit-field-input");
+            label.AddToClassList("thunderkit-field-label");
+            return container;
+        }
 
     }
 }
