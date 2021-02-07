ff2698215f26d28ebac8b481cb7e1e0acb6abedc
PassivePicasso <james.lapenn@gmail.com>
2021-02-06 22:33:51 -0500

    Try using directory marking assets to define forbidden bundles at a project level

---
+++ Core/ForbiddenAssetBundle.cs
@@ -0,0 +1,12 @@
+﻿using ThunderKit.Core.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace ThunderKit.Core
+{
+    public class ForbiddenAssetBundle : ScriptableObject
+    {
+        [MenuItem(Constants.ThunderKitContextRoot + "Forbidden AssetBundle", priority = Constants.ThunderKitMenuPriority)]
+        public static void Create() => ScriptableHelper.SelectNewAsset<ForbiddenAssetBundle>();
+    }
+}
