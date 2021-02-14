a97cd62f48a32c4e1cb1411b6184cacac4de6c46
PassivePicasso <james.lapenn@gmail.com>
2021-02-14 14:44:29 -0500

    re-organize code for improved re-usability

--- Core/Editor/ScriptEditorHelper.cs
+++ Core/Editor/ScriptEditorHelper.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using ThunderKit.Core.Editor.Controls;
+using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
 
@@ -14,5 +16,40 @@ namespace ThunderKit.Core.Editor
             InternalEditorUtility.OpenFileAtLineExternal(scriptPath, -1);
 
         }
+        public static (string destinationPath, string nameSpace, string fileName) GetDetails(string typeFullName) => (
+                          destinationPath: Path.Combine("Assets", $"{typeFullName}.cs").Replace("\\", "/"),
+                          nameSpace: Path.GetDirectoryName(typeFullName).Replace('/', '.').Replace('\\', '.'),
+                          fileName: Path.GetFileNameWithoutExtension(typeFullName)
+                      );
+
+        /// <summary>
+        /// Generates a new script at the specified path.
+        /// Type will generate with a namespace equal to the given path
+        /// </summary>
+        /// <param name="template"></param>
+        /// <param name="newScriptPath">Path relative to the project's Assets folder</param>
+        public static void GenerateAndLoadScript(string template, string newScriptPath)
+        {
+            var (destinationPath, ns, fileName) = GetDetails(newScriptPath);
+
+            if (string.IsNullOrEmpty(template)) return;
+
+            var backup = NewScriptInfo.Instance;
+            backup.addAsset = true;
+            backup.scriptPath = destinationPath;
+            EditorUtility.SetDirty(backup);
+
+            var parentDirectoryPath = Path.GetDirectoryName(destinationPath);
+            Directory.CreateDirectory(parentDirectoryPath);
+
+            ns = string.IsNullOrEmpty(ns) ? "Assets" : ns;
+            var rendered = string.Format(template, ns, fileName);
+            File.WriteAllText(destinationPath, rendered);
+
+            AssetDatabase.ImportAsset(destinationPath);
+            AssetDatabase.Refresh();
+
+        }
+
     }
 }
