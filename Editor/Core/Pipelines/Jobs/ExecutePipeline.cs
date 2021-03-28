887e8d4235bf6cb74da74e4611af3170a7152ff3
PassivePicasso <james.lapenn@gmail.com>
2021-03-28 15:17:12 -0400

    add disabled copy config step to deploy pipeline
    update Launch pipeline to only execute the game
    add Rebuild and Launch to execute Stage, Deploy and Launch in sequence

--- Editor/Core/Pipelines/Jobs/ExecutePipeline.cs
+++ Editor/Core/Pipelines/Jobs/ExecutePipeline.cs
@@ -5,13 +5,15 @@ namespace ThunderKit.Core.Pipelines.Jobs
     [PipelineSupport(typeof(Pipeline))]
     public class ExecutePipeline : PipelineJob
     {
-        public bool InheritRootManifest;
+        public bool OverrideManifest;
         public Pipeline executePipeline;
         public override void Execute(Pipeline pipeline)
         {
             if (!executePipeline) return;
 
-            if (InheritRootManifest && pipeline.manifest)
+            // pipeline.manifest is the correct field to use, stop checking every time.
+            // pipieline.manifest is the manifest that is assigned to the pipeline via the editor
+            if (OverrideManifest && pipeline.manifest) 
             {
                 var manifest = executePipeline.manifest;
                 executePipeline.manifest = pipeline.manifest;
