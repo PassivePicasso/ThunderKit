13a0bab0d69379d8b4c45a715539dbd94bed068e
PassivePicasso <james.lapenn@gmail.com>
2021-05-24 23:33:15 -0400

    fix additional case where runaway processing can occur

--- Editor/Integrations/Thunderstore/ThunderstoreAPI.cs
+++ Editor/Integrations/Thunderstore/ThunderstoreAPI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using ThunderKit.Core.Data;
@@ -14,37 +15,43 @@ namespace ThunderKit.Integrations.Thunderstore
     /// </summary>
     public class ThunderstoreAPI
     {
+        internal class ThunderstoreLoadBehaviour : MonoBehaviour { }
+
         public static List<PackageListing> LoadedPages;
 
         static string PackageListApi => ThunderKitSetting.GetOrCreateSettings<ThunderstoreSettings>().ThunderstoreUrl + "/api/v1/package/";
 
-        public static void ReloadPages()
+        static IEnumerator LoadPackageListings()
         {
-            Debug.Log($"Updating Package listing against {PackageListApi}");
             var packages = new List<PackageListing>();
             var webRequest = UnityWebRequest.Get(PackageListApi);
-            var asyncOpRequest = webRequest.SendWebRequest();
+            yield return webRequest.SendWebRequest();
 
-            asyncOpRequest.completed += (obj) =>
-            {
-                var response = string.Empty;
+            var response = string.Empty;
 
 #if UNITY_2020_1_OR_NEWER
                 if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
 #else
-                if (webRequest.isNetworkError || webRequest.isHttpError)
+            if (webRequest.isNetworkError || webRequest.isHttpError)
 #endif
-                    Debug.Log(webRequest.error);
-                else
-                {
-                    response = webRequest.downloadHandler.text;
+                Debug.Log(webRequest.error);
+            else
+            {
+                response = webRequest.downloadHandler.text;
 
-                    var resultSet = JsonUtility.FromJson<PackagesResponse>($"{{ \"{nameof(PackagesResponse.results)}\": {response} }}");
-                    packages.AddRange(resultSet.results);
-                    LoadedPages = packages;
-                    Debug.Log($"Package listing update: {PackageListApi}");
-                }
-            };
+                var resultSet = JsonUtility.FromJson<PackagesResponse>($"{{ \"{nameof(PackagesResponse.results)}\": {response} }}");
+                packages.AddRange(resultSet.results);
+                LoadedPages = packages;
+                Debug.Log($"Package listing update: {PackageListApi}");
+            }
+        }
+
+        public static void ReloadPages()
+        {
+            Debug.Log($"Updating Package listing against {PackageListApi}");
+            var imageLoaderObject = new GameObject("ThunderstoreDataLoader", typeof(ThunderstoreLoadBehaviour)) { isStatic = true, hideFlags = HideFlags.HideAndDontSave };
+            var imageLoader = imageLoaderObject.GetComponent<ThunderstoreLoadBehaviour>();
+            imageLoader.StartCoroutine(LoadPackageListings());
         }
 
         public static IEnumerable<PackageListing> LookupPackage(string name)
