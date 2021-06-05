4cba34bb6e5cc7201e546016ac5cb5b6ee7c8070
PassivePicasso <james.lapenn@gmail.com>
2021-06-05 16:21:10 -0400

    Add "Documentation Folder" and "Documentation Page" items to ThunderKit Context Menu
    These two new context items will create a Documentation Folder and Page respectively.
    If created under the ThunderKit/Documentation/topics folder they will be added to the default ThunderKit Documentation Window under the appropriate location.
    A new Documentation window can be configured to point at different roots.

--- Editor/Core/Documentation/DocumentationHelpers.cs
+++ Editor/Core/Documentation/DocumentationHelpers.cs
@@ -13,8 +13,8 @@ namespace ThunderKit.Core.Documentation
     {
         readonly static object[] findTextureParams = new object[1];
 
-        [MenuItem(Constants.ThunderKitContextRoot + "Documentation Page", priority = Constants.ThunderKitMenuPriority)]
-        public static void Create()
+        [MenuItem(Constants.ThunderKitContextRoot + "Documentation Folder", priority = Constants.ThunderKitMenuPriority)]
+        public static void CreateFolder()
         {
             string path = AssetDatabase.GetAssetPath(Selection.activeObject);
             if (path == "")
@@ -25,7 +25,48 @@ namespace ThunderKit.Core.Documentation
             {
                 path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
             }
-            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath($"{path}/MarkdownFile.md");
+            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath($"NewDocumentationFolder.md");
+
+            AssetDatabase.Refresh();
+
+            Action<int, string, string> action =
+                (int instanceId, string markPath, string resourceFile) =>
+                {
+                    var name = Path.GetFileNameWithoutExtension(markPath);
+                    var rootPath = Path.GetDirectoryName(markPath);
+
+                    var directory = Path.Combine(rootPath, name);
+                    var uxmlPath = Path.Combine(rootPath, $"{name}.uxml");
+                    var title = ObjectNames.NicifyVariableName(name);
+
+                    Directory.CreateDirectory(directory);
+                    File.WriteAllText(markPath, $"# {title}");
+                    File.WriteAllText(uxmlPath, GetUXMLTemplate(title));
+                    AssetDatabase.Refresh();
+                    Selection.activeObject = AssetDatabase.LoadAssetAtPath<TextAsset>(markPath);
+                };
+
+            var endAction = ScriptableObject.CreateInstance<SelfDestructingActionAsset>();
+            endAction.action = action;
+            var findTexture = typeof(EditorGUIUtility).GetMethod(nameof(EditorGUIUtility.FindTexture), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            findTextureParams[0] = typeof(DefaultAsset);
+            var icon = (Texture2D)findTexture.Invoke(null, findTextureParams);
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, endAction, assetPathAndName, icon, null);
+        }
+
+        [MenuItem(Constants.ThunderKitContextRoot + "Documentation Page", priority = Constants.ThunderKitMenuPriority)]
+        public static void CreatePage()
+        {
+            string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            if (path == "")
+            {
+                path = "Assets";
+            }
+            else if (Path.GetExtension(path) != "")
+            {
+                path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+            }
+            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath($"NewDocumentationPage");
 
             AssetDatabase.Refresh();
 
