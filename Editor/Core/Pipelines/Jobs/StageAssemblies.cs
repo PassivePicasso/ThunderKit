0765d46209981693983d16561b3a0f5f5e22a9c9
PassivePicasso <james.lapenn@gmail.com>
2021-09-05 11:57:06 -0400

    Re-impelement StageAssemblies to allow control of building processes Allow Pipelines to run without manifests Add support for BepInEx configs in the BepInEx Templates

--- Editor/Core/Pipelines/Jobs/StageAssemblies.cs
+++ Editor/Core/Pipelines/Jobs/StageAssemblies.cs
@@ -1,9 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using ThunderKit.Core.Attributes;
 using ThunderKit.Core.Data;
 using ThunderKit.Core.Manifests.Datums;
 using ThunderKit.Core.Paths;
+using UnityEditor;
+using UnityEditor.Compilation;
 using UnityEditorInternal;
 using UnityEngine;
 
@@ -12,54 +17,145 @@ namespace ThunderKit.Core.Pipelines.Jobs
     [PipelineSupport(typeof(Pipeline)), ManifestProcessor]
     public class StageAssemblies : PipelineJob
     {
-        public bool stageDebugDatabases;
-        public bool preferPlayerBuilds;
-
-        public override void Execute(Pipeline pipeline)
+        public static readonly HashSet<AssemblyBuilder> BuildStatus = new HashSet<AssemblyBuilder>();
+        static string Combine(params string[] component) => Path.Combine(component).Replace('\\', '/');
+#pragma warning disable CS0649 
+        struct AsmBuildData : IEquatable<AsmBuildData>
         {
-            foreach (var asmDefs in pipeline.Manifest.Data.OfType<AssemblyDefinitions>())
-                foreach (var outputPath in asmDefs.StagingPaths.Select(path => path.Resolve(pipeline, this)))
-                    CopyReferences(asmDefs.definitions, outputPath);
-        }
+            public string Directory;
+            public string AssemblyDefinition;
+            public string Output;
+            public string[] Scripts;
 
-        void CopyReferences(AssemblyDefinitionAsset[] assemblyDefs, string outputPath)
-        {
-            var scriptAssemblies = Path.Combine("Library", "ScriptAssemblies");
-            var playerScriptAssemblies = Path.Combine("Library", "PlayerScriptAssemblies");
-
-            foreach (var definition in assemblyDefs)
+            public override bool Equals(object obj)
             {
-                var assemblyDef = JsonUtility.FromJson<AssemblyDef>(definition.text);
-                var fileOutputBase = Path.Combine(outputPath, assemblyDef.name);
-                var fileName = Path.GetFileName(fileOutputBase);
-                Directory.CreateDirectory(outputPath);
-                if (stageDebugDatabases)
+                return obj is AsmBuildData data && Equals(data);
+            }
+
+            public bool Equals(AsmBuildData other)
+            {
+                return Directory == other.Directory &&
+                       AssemblyDefinition == other.AssemblyDefinition;
+            }
+
+            public override int GetHashCode()
+            {
+                int hashCode = -815763736;
+                hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Directory);
+                hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(AssemblyDefinition);
+                return hashCode;
+            }
+
+            public static bool operator ==(AsmBuildData left, AsmBuildData right)
+            {
+                return left.Equals(right);
+            }
+
+            public static bool operator !=(AsmBuildData left, AsmBuildData right)
+            {
+                return !(left == right);
+            }
+        }
+#pragma warning restore CS0649 
+
+        public bool stageDebugDatabases;
+        [PathReferenceResolver, Tooltip("Location where built assemblies will be cached before being staged")]
+        public string assemblyArtifactPath = "<AssemblyStaging>";
+        public BuildTarget buildTarget = BuildTarget.StandaloneWindows;
+        public BuildTargetGroup buildTargetGroup = BuildTargetGroup.Standalone;
+
+
+        public sealed override void Execute(Pipeline pipeline)
+        {
+            var resolvedArtifactPath = PathReference.ResolvePath(assemblyArtifactPath, pipeline, this);
+            Directory.CreateDirectory(resolvedArtifactPath);
+
+            var assemblies = CompilationPipeline.GetAssemblies();
+            var definitions = pipeline.Manifest.Data
+                .OfType<AssemblyDefinitions>()
+                .SelectMany(def => def.definitions.Select(d => (asm: assemblies.FirstOrDefault(asm => d.name == asm.name), asmDefs: def)))
+                .Where(def => def.asm != null)
+                .ToArray();
+
+            foreach (var definition in definitions)
+            {
+                var assemblyName = $"{definition.asm.name}.dll";
+                var targetName = Path.GetFileNameWithoutExtension(definition.asm.name);
+                var assemblyOutputPath = Combine(resolvedArtifactPath, assemblyName);
+                var builder = new AssemblyBuilder(assemblyOutputPath, definition.asm.sourceFiles)
                 {
-                    CopyFiles(scriptAssemblies, outputPath, $"{fileName}*.pdb", $"{fileName}*.mdb", $"{fileName}*.dll");
-                    if (preferPlayerBuilds)
-                        CopyFiles(playerScriptAssemblies, outputPath, $"{fileName}*.pdb", $"{fileName}*.mdb", $"{fileName}*.dll");
-                }
-                else
+                    additionalReferences = definition.asm.allReferences,
+                };
+                builder.excludeReferences = builder.defaultReferences.Where(rf => rf.Contains(assemblyName)).ToArray();
+                builder.buildTargetGroup = buildTargetGroup;
+                
+                var index = pipeline.ManifestIndex;
+                void OnBuildStarted(string path) => Debug.Log($"Building : {path}");
+                void OnBuildFinished(string path, CompilerMessage[] messages)
                 {
-                    CopyFiles(scriptAssemblies, outputPath, $"{fileName}*.dll");
-                    if (preferPlayerBuilds)
-                        CopyFiles(playerScriptAssemblies, outputPath, $"{fileName}*.dll");
+                    BuildStatus.Remove(builder);
+                    if (messages.Any())
+                        foreach (var message in messages.OrderBy(msg => msg.type))
+                        {
+                            switch (message.type)
+                            {
+                                case CompilerMessageType.Error:
+                                    Debug.LogError(message.message);
+                                    break;
+                                case CompilerMessageType.Warning:
+                                    Debug.LogWarning(message.message);
+                                    break;
+                            }
+                        }
+                    else
+                        Debug.Log($"Build Completed: {path}");
+
+                    Debug.Log($"Resolving Paths: {path}");
+                    var prevIndex = pipeline.ManifestIndex;
+                    pipeline.ManifestIndex = index;
+                    var resolvedPaths = definition.asmDefs.StagingPaths
+                        .Select(p => PathReference.ResolvePath(p, pipeline, this)).ToArray();
+                    pipeline.ManifestIndex = prevIndex;
+                    Debug.Log($"Resolved Paths: {path}");
+
+
+                    foreach (var outputPath in resolvedPaths)
+                    {
+
+                        Debug.Log($"Copying {assemblyName} to {outputPath}");
+                        Directory.CreateDirectory(outputPath);
+                        if (stageDebugDatabases)
+                            CopyFiles(resolvedArtifactPath, outputPath, $"{targetName}*.pdb", $"{targetName}*.mdb", assemblyName);
+                        else
+                            CopyFiles(resolvedArtifactPath, outputPath, assemblyName);
+                    }
                 }
+                builder.buildTarget = buildTarget;
+                builder.buildFinished += OnBuildFinished;
+                builder.buildStarted += OnBuildStarted;
+
+                if (File.Exists(assemblyOutputPath))
+                    File.Delete(assemblyOutputPath);
+                BuildStatus.Add(builder);
+                builder.Build();
             }
         }
 
-
-        void CopyFiles(string path, string outputPath, params string[] patterns)
+        void CopyFiles(string sourcePath, string outputPath, params string[] patterns)
         {
             Directory.CreateDirectory(outputPath);
-            var targetFiles = from pattern in patterns
-                              from file in Directory.GetFiles(path, pattern, SearchOption.AllDirectories)
-                              select file;
-            foreach (var file in targetFiles)
+            var targetFiles = (from pattern in patterns
+                              from file in Directory.GetFiles(sourcePath, pattern, SearchOption.AllDirectories)
+                              select file).ToArray();
+            foreach (var source in targetFiles)
             {
-                var fileName = Path.GetFileName(file);
-                File.Copy(file, Path.Combine(outputPath, fileName), true);
+                var fileName = Path.GetFileName(source);
+                string destination = Combine(outputPath, fileName);
+                File.Copy(source, destination, true);
+                Debug.Log($"Copied {source} to {destination}");
+
             }
         }
+
     }
 }
