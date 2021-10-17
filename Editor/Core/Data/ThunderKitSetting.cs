504bb3a00e264785aebc9e1d32c9b5aee1871bc6
PassivePicasso <james.lapenn@gmail.com>
2021-10-17 14:56:50 -0400

    Fix mistake in attempt at preventing ThunderKitSetting from being created

--- Editor/Core/Data/ThunderKitSetting.cs
+++ Editor/Core/Data/ThunderKitSetting.cs
@@ -16,10 +16,11 @@ using UnityEngine.Experimental.UIElements;
 
 namespace ThunderKit.Core.Data
 {
-    public class ThunderKitSetting : ScriptableObject
+    public abstract class ThunderKitSetting : ScriptableObject
     {
         static Type[] thunderKitSettingsTypes = null;
 
+        [InitializeOnLoadMethod]
         static void Ensure()
         {
             SettingsWindow.OnSettingsLoading -= Ensure;
@@ -74,9 +75,6 @@ namespace ThunderKit.Core.Data
         public virtual void Initialize() { }
         public virtual IEnumerable<string> Keywords() => Enumerable.Empty<string>();
         public virtual void CreateSettingsUI(VisualElement rootElement) { }
-
-        protected static Type[] createSettingsUiParameterTypes = new[] { typeof(VisualElement) };
-
         protected static VisualElement CreateStandardField(string gamePath)
         {
             var container = new VisualElement();
