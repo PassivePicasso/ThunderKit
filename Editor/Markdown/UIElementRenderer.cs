06582a7993c1bd132d13e43913b3d7e60e6496a5
PassivePicasso <james.lapenn@gmail.com>
2021-10-25 17:06:53 -0400

    Fix some issues with Markdown Rendering
    MarkDig was trying to render HTML elements with features not available in the Unity environment. This has been fixed by forcing Markdig to render <Tags> as plain text instead of attempting to render HTML.
    This feature May be revisited for rendering VisualElements from tags later.
    the UIElementRenderer code has bee consolidated and cleaned up

--- Editor/Markdown/UIElementRenderer.cs
+++ Editor/Markdown/UIElementRenderer.cs
@@ -25,20 +25,14 @@ namespace ThunderKit.Markdown
         private static readonly Regex LiteralSplitter = new Regex(@"([\S]+\b)\S?", RegexOptions.Singleline | RegexOptions.Compiled);
         private readonly Stack<VisualElement> stack = new Stack<VisualElement>(128);
 
-        public UIElementRenderer()
-        {
-        }
-
-        public UIElementRenderer(VisualElement document)
-        {
-            LoadDocument(document);
-        }
+        public UIElementRenderer() { }
         public virtual void LoadDocument(VisualElement document)
         {
             Document = document;
             stack.Push(document);
             LoadRenderers();
         }
+
         public VisualElement Document { get; protected set; }
         /// <inheritdoc/>
         public override object Render(MarkdownObject markdownObject)
@@ -47,16 +41,6 @@ namespace ThunderKit.Markdown
             return Document;
         }
 
-        public void WriteLeafInline(LeafBlock leafBlock)
-        {
-            if (leafBlock == null) throw new ArgumentNullException(nameof(leafBlock));
-            var inline = (Inline)leafBlock.Inline;
-            while (inline != null)
-            {
-                Write(inline);
-                inline = inline.NextSibling;
-            }
-        }
         public void WriteLeafRawLines(LeafBlock leafBlock)
         {
             if (leafBlock == null) throw new ArgumentNullException(nameof(leafBlock));
@@ -68,7 +52,7 @@ namespace ThunderKit.Markdown
                 {
                     if (i != 0)
                         WriteInline(GetClassedElement<Label>("linebreak"));
-                    WriteText(ref slices[i].Slice);
+                    WriteSplitText(ref slices[i].Slice);
                 }
             }
         }
@@ -81,15 +65,11 @@ namespace ThunderKit.Markdown
             var popped = stack.Pop();
             stack.Peek().Add(popped);
         }
-        public void WriteBlock(VisualElement block)
-        {
-            stack.Peek().Add(block);
-        }
         public void WriteInline(VisualElement inline)
         {
             stack.Peek().Add(inline);
         }
-        public void WriteText(ref StringSlice slice)
+        public void WriteSplitText(ref StringSlice slice)
         {
             if (slice.IsEmpty)
                 return;
@@ -103,8 +83,8 @@ namespace ThunderKit.Markdown
                     var element = GetTextElement<Label>(value, "inline");
 
                     match = match.NextMatch();
-                    if (match.Success == false)
-                        element.AddToClassList("last-inline");
+                    //if (match.Success == false)
+                    //    element.AddToClassList("last-inline");
 
                     WriteInline(element);
                 }
@@ -118,35 +98,31 @@ namespace ThunderKit.Markdown
         public void WriteOptimizedLeafInline(LeafBlock block)
         {
             var inline = block.Inline.FirstChild;
+            Inline previousInline = null;
             var splitLiterals = false;
             var builder = new StringBuilder();
-            while (inline != null)
+            while (inline != null && inline != previousInline)
             {
+                previousInline = inline;
                 switch (inline)
                 {
-                    case CodeInline codeInline:
-                    case LinkInline linkInline:
-                    case EmphasisInline emphasisInline:
+                    case CodeInline _:
+                    case LinkInline _:
+                    case EmphasisInline _:
                         splitLiterals = true;
-                        inline = null;
                         break;
-                    case HtmlEntityInline htmlEntityInline:
-                        builder.Append(htmlEntityInline.Transcoded);
-                        inline = htmlEntityInline.NextSibling;
+                    case LineBreakInline _:
+                        builder.AppendLine();
                         break;
                     case HtmlInline htmlInline:
                         builder.Append(htmlInline.Tag);
-                        inline = htmlInline.NextSibling;
-                        break;
-                    case LineBreakInline lineBreakInline:
-                        builder.AppendLine();
-                        inline = inline.NextSibling;
                         break;
                     default:
-                        builder.Append(inline);
-                        inline = inline.NextSibling;
+                        builder.Append($"{inline}");
                         break;
                 }
+                inline = inline.NextSibling;
+                if (splitLiterals) break;
             }
             if (!splitLiterals)
             {
@@ -160,22 +136,35 @@ namespace ThunderKit.Markdown
             }
         }
 
+        public void WriteLeafInline(LeafBlock leafBlock)
+        {
+            if (leafBlock == null) throw new ArgumentNullException(nameof(leafBlock));
+            var inline = leafBlock.Inline.FirstChild;
+            while (inline != null)
+            {
+                switch (inline)
+                {
+                    case HtmlInline htmlInline:
+                        WriteText(htmlInline.Tag);
+                        break;
+                    case HtmlEntityInline htmlEntityInline:
+                        WriteText(htmlEntityInline.ToString());
+                        break;
+                    default:
+                        Write(inline);
+                        break;
+                }
+                inline = inline.NextSibling;
+            }
+        }
+
         public void WriteText(string text)
         {
             if (text == null)
                 return;
 
             var slice = new StringSlice(text);
-            WriteText(ref slice);
-        }
-
-        public void WriteText(string text, int offset, int length)
-        {
-            if (text == null)
-                return;
-
-            var slice = new StringSlice(text, offset, offset + length);
-            WriteText(ref slice);
+            WriteSplitText(ref slice);
         }
 
         protected virtual void LoadRenderers()
@@ -187,17 +176,16 @@ namespace ThunderKit.Markdown
             ObjectRenderers.Add(new ParagraphRenderer());
             ObjectRenderers.Add(new QuoteBlockRenderer());
             ObjectRenderers.Add(new ThematicBreakRenderer());
+
             // Default inline renderers
-            ObjectRenderers.Add(new AutolinkInlineRenderer());
             ObjectRenderers.Add(new CodeInlineRenderer());
             ObjectRenderers.Add(new DelimiterInlineRenderer());
             ObjectRenderers.Add(new EmphasisInlineRenderer());
-            ObjectRenderers.Add(new HtmlEntityInlineRenderer());
             ObjectRenderers.Add(new LineBreakInlineRenderer());
             ObjectRenderers.Add(new LinkInlineRenderer());
             ObjectRenderers.Add(new LiteralInlineRenderer());
+
             // Extension renderers
-            //ObjectRenderers.Add(new TableRenderer());
             ObjectRenderers.Add(new TaskListRenderer());
         }
     }
