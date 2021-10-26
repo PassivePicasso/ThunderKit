17f175fc435e6300a09f665199ea0c12206c202e
PassivePicasso <james.lapenn@gmail.com>
2021-10-25 22:19:23 -0400

    remove save assets call in pipeline

--- Editor/Core/Pipelines/Pipeline.cs
+++ Editor/Core/Pipelines/Pipeline.cs
@@ -159,7 +159,6 @@ namespace {0}
             finally
             {
                 EditorUtility.SetDirty(Logger);
-                AssetDatabase.SaveAssets();
                 Logger = null;
             }
         }
