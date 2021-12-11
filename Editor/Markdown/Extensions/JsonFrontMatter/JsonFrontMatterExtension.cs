4ad6c9652f7de5f34168c2a52306425c26e1f89f
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-11 17:36:54 -0500

    Add JsonFrontMatter extension to Markdown engine
    Adding a JsonFrontMatter extension to the Markdown Engine in a static form.  Will consider configurable design in the future for advanced scenarios.
    The JsonFrontMatter in its current form will allow the configuration of a MarkdownElement to some extent from the FrontMatter of a Markdown document.
    This allows the author to enable a header on the document with custom classes applied to its elements.
    The header is a container, which contains an icon and a label.
    A USS style sheet can be added to the MarkdownElement from the FrontMatter
    Classes can be added to the header container, Icon, or label from the FrontMatter as well.

---
+++ Editor/Markdown/Extensions/JsonFrontMatter/JsonFrontMatterExtension.cs
@@ -0,0 +1,29 @@
+﻿using Markdig;
+using Markdig.Renderers;
+
+namespace ThunderKit.Markdown.Extensions.Json
+{
+    /// <summary>
+    /// Extension to discard a YAML frontmatter at the beginning of a Markdown document.
+    /// </summary>
+    public class JsonFrontMatterExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            if (!pipeline.BlockParsers.Contains<YamlFrontMatterParser>())
+            {
+                // Insert the YAML parser before the thematic break parser, as it is also triggered on a --- dash
+                //pipeline.BlockParsers.InsertBefore<ThematicBreakParser>(new YamlFrontMatterParser());
+                pipeline.BlockParsers.Insert(0, new YamlFrontMatterParser());
+            }
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+            if (!renderer.ObjectRenderers.Contains<JsonFrontMatterRenderer>())
+            {
+                renderer.ObjectRenderers.Insert(0, new JsonFrontMatterRenderer());
+            }
+        }
+    }
+}
