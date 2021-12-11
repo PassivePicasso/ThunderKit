c4f3d27c2b7e68fe00b8de7391a1abe96f361ebb
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-11 17:37:32 -0500

    Add extensions class for handling some features not available in Markdig 18.3

---
+++ Editor/Markdown/Helpers/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using Markdig.Helpers;
+using System.Runtime.CompilerServices;
+
+namespace ThunderKit.Markdown.Helpers
+{
+    public static class Extensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int CountAndSkipChar(this StringSlice slice, char matchChar)
+        {
+            string text = slice.Text;
+            int end = slice.End;
+            int current = slice.Start;
+
+            while (current <= end && (uint)current < (uint)text.Length && text[current] == matchChar)
+            {
+                current++;
+            }
+
+            int count = current - slice.Start;
+            slice.Start = current;
+            return count;
+        }
+    }
+}
