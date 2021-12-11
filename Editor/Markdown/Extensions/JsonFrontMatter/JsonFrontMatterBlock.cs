eadf81438445643952ae5cd922974ac3d55fbb64
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-11 17:36:54 -0500

    Add JsonFrontMatter extension to Markdown engine
    Adding a JsonFrontMatter extension to the Markdown Engine in a static form.  Will consider configurable design in the future for advanced scenarios.
    The JsonFrontMatter in its current form will allow the configuration of a MarkdownElement to some extent from the FrontMatter of a Markdown document.
    This allows the author to enable a header on the document with custom classes applied to its elements.
    The header is a container, which contains an icon and a label.
    A USS style sheet can be added to the MarkdownElement from the FrontMatter
    Classes can be added to the header container, Icon, or label from the FrontMatter as well.

---
+++ Editor/Markdown/Extensions/JsonFrontMatter/JsonFrontMatterBlock.cs
@@ -0,0 +1,12 @@
+﻿using Markdig.Parsers;
+using Markdig.Syntax;
+
+namespace ThunderKit.Markdown.Extensions.Json
+{
+    public class JsonFrontMatterBlock : CodeBlock
+    {
+        public JsonFrontMatterBlock(BlockParser parser) : base(parser)
+        {
+        }
+    }
+}
---
+++ Editor/Markdown/Extensions/JsonFrontMatter/JsonFrontMatterRenderer.cs
@@ -0,0 +1,69 @@
+﻿using ThunderKit.Markdown.ObjectRenderers;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEditor;
+using System.IO;
+#if UNITY_2019_1_OR_NEWER
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+#else
+using UnityEngine.Experimental.UIElements.StyleSheets;
+using UnityEngine.Experimental.UIElements;
+using UnityEditor.Experimental.UIElements;
+#endif
+
+namespace ThunderKit.Markdown.Extensions.Json
+{
+    using static Helpers.VisualElementFactory;
+    using static Helpers.UnityPathUtility;
+    public class JsonFrontMatterRenderer : UIElementObjectRenderer<JsonFrontMatterBlock>
+    {
+        public struct FrontMatter
+        {
+            public string pageStylePath;
+
+            public string[] headerClasses;
+
+            public string content;
+            public string[] contentClasses;
+
+            public string iconUrl;
+            public string[] iconClasses;
+        }
+        protected override void Write(UIElementRenderer renderer, JsonFrontMatterBlock frontMatterBlock)
+        {
+            var json = frontMatterBlock.Lines.ToString();
+            var frontMatter = JsonUtility.FromJson<FrontMatter>(json);
+
+            var header = GetClassedElement<VisualElement>(frontMatter.headerClasses);
+            header.name = nameof(header);
+
+            if (!string.IsNullOrEmpty(frontMatter.iconUrl))
+            {
+                var headerIcon = GetImageElement<Image>(frontMatter.iconUrl, frontMatter.iconClasses);
+                headerIcon.name = nameof(headerIcon);
+                header.Add(headerIcon);
+            }
+            if (!string.IsNullOrEmpty(frontMatter.content))
+            {
+                var title = GetTextElement<Label>(frontMatter.content, frontMatter.contentClasses);
+                title.name = nameof(title);
+                header.Add(title);
+            }
+            else if (IsAssetDirectory(renderer.Document.Data))
+            {
+                var fileName = Path.GetFileNameWithoutExtension(renderer.Document.Data);
+                fileName = ObjectNames.NicifyVariableName(fileName);
+                var title = GetTextElement<Label>(fileName, frontMatter.contentClasses);
+                title.name = nameof(title);
+                header.Add(title);
+            }
+
+            renderer.WriteElement(header);
+            var parent = header.parent;
+            if (!parent.HasStyleSheetPath(frontMatter.pageStylePath))
+                parent.AddStyleSheetPath(frontMatter.pageStylePath);
+        }
+    }
+}
