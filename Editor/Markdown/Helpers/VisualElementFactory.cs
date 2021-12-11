34893f364e0202d5d7e12049746ce30f4dab107c
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-11 17:32:59 -0500

    Move Image Element creation system into VisualElementFactory for re-use
    Image elements require a number of features to enable, move this code into VisualElementFactory for ease of use

--- Editor/Markdown/Helpers/VisualElementFactory.cs
+++ Editor/Markdown/Helpers/VisualElementFactory.cs
@@ -1,19 +1,74 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using ThunderKit.Markdown.ObjectRenderers;
+using UnityEditor;
 #if UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 #else
 using UnityEngine.Experimental.UIElements.StyleSheets;
 using UnityEngine.Experimental.UIElements;
-using UnityEditor.Experimental.UIElements;
 #endif
 
 namespace ThunderKit.Markdown.Helpers
 {
-    public static class VisualElementFactory 
+    using static Helpers.UnityPathUtility;
+    public static class VisualElementFactory
     {
+        internal class ImageLoadBehaviour : MonoBehaviour { }
+        public static T GetImageElement<T>(string url, params string[] classNames) where T : Image, new()
+        {
+            T imageElement = GetClassedElement<T>(classNames);
+            if (IsAssetDirectory(url))
+            {
+                var image = AssetDatabase.LoadAssetAtPath<Texture>(url);
+                if (image)
+                    SetupImage(imageElement, image);
+            }
+            else
+            {
+                var imageLoaderObject = new GameObject("MarkdownImageLoader", typeof(ImageLoadBehaviour)) { isStatic = true, hideFlags = HideFlags.HideAndDontSave };
+                var imageLoader = imageLoaderObject.GetComponent<ImageLoadBehaviour>();
+                var c = imageLoader.StartCoroutine(LoadImage(url, imageElement));
+            }
+            return imageElement;
+        }
+        static IEnumerator LoadImage(string url, Image imageElement)
+        {
+            using (var request = UnityWebRequestTexture.GetTexture(url))
+            {
+                imageElement.RegisterCallback<DetachFromPanelEvent, UnityWebRequest>(CancelRequest, request);
+
+                yield return request.SendWebRequest();
+
+#if UNITY_2020_1_OR_NEWER
+                if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+#else
+                if (request.isNetworkError || request.isHttpError)
+#endif
+                    Debug.Log(request.error);
+                else
+                    SetupImage(imageElement, ((DownloadHandlerTexture)request.downloadHandler).texture);
+
+                imageElement.UnregisterCallback<DetachFromPanelEvent, UnityWebRequest>(CancelRequest);
+            }
+        }
+        static void CancelRequest(DetachFromPanelEvent evt, UnityWebRequest webRequest) => webRequest.Abort();
+
+        static void SetupImage(Image imageElement, Texture texture)
+        {
+            imageElement.image = texture;
+#if UNITY_2019_1_OR_NEWER
+            imageElement.style.width = texture.width;
+            imageElement.style.height = texture.height;
+#else
+            imageElement.style.width = new StyleValue<float>(texture.width);
+            imageElement.style.height = new StyleValue<float>(texture.height);
+#endif
+        }
+
+
         public static T GetTextElement<T>(string text, string className) where T : TextElement, new()
         {
             T element = GetClassedElement<T>(className);
