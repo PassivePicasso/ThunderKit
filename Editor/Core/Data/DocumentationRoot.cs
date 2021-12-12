1eb98f9c9db103d494c132172b2431fad652cb43
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-11 23:18:23 -0500

    Add asset to mark documentation root paths

---
+++ Editor/Core/Data/DocumentationRoot.cs
@@ -0,0 +1,16 @@
+﻿using ThunderKit.Common;
+using UnityEditor;
+using UnityEngine;
+
+namespace ThunderKit.Core.Data
+{
+    using static ScriptableHelper;
+    public class DocumentationRoot : ScriptableObject
+    {
+        [MenuItem(Constants.ThunderKitContextRoot + nameof(DocumentationRoot), false, priority = Constants.ThunderKitMenuPriority)]
+        public static void Create()
+        {
+            SelectNewAsset<DocumentationRoot>();
+        }
+    }
+}
