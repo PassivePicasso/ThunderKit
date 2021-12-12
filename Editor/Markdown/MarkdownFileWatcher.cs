9abf3d7932cbae9737474fc9c3b63d82ad3c1f2f
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-11 19:05:04 -0500

    Implement MarkdownFileWatcher
    the MarkdownFileWatcher will listen for Unity Imports and when a Markdown file is imported it is checked against a set of registered MarkdownElements and update any which point to an imported file.

---
+++ Editor/Markdown/MarkdownFileWatcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace ThunderKit.Markdown
+{
+    public class MarkdownFileWatcher : AssetPostprocessor
+    {
+        static Dictionary<string, HashSet<MarkdownElement>> ActiveElementDocuments = new Dictionary<string, HashSet<MarkdownElement>>();
+        public static void RegisterActiveElement(string document, MarkdownElement element)
+        {
+            if (!ActiveElementDocuments.ContainsKey(document))
+                ActiveElementDocuments[document] = new HashSet<MarkdownElement>();
+
+            ActiveElementDocuments[document].Add(element);
+        }
+        public static void UnregisterActiveElement(string document, MarkdownElement element)
+        {
+            if (!ActiveElementDocuments.ContainsKey(document)) return;
+
+            ActiveElementDocuments[document].Remove(element);
+        }
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            foreach (string document in importedAssets)
+            {
+                if (Path.GetExtension(document) != ".md") continue;
+                if (!ActiveElementDocuments.ContainsKey(document)) continue;
+                foreach (var element in ActiveElementDocuments[document])
+                    element.RefreshContent();
+            }
+        }
+    }
+}
