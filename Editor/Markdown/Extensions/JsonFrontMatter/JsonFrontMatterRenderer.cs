8c7d9ed39b65c00442712e420f39e45f8efc08bd
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-11 19:07:33 -0500

    Add support for Content Redirection via FrontMatter
    Json FrontMatter will now add a child MarkdownElement pointing at contentUrl when it is populated with a valid path.

--- Editor/Markdown/Extensions/JsonFrontMatter/JsonFrontMatterRenderer.cs
+++ Editor/Markdown/Extensions/JsonFrontMatter/JsonFrontMatterRenderer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using UnityEngine.Networking;
 using UnityEditor;
 using System.IO;
+using System;
 #if UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
@@ -25,45 +26,64 @@ namespace ThunderKit.Markdown.Extensions.Json
 
             public string[] headerClasses;
 
-            public string content;
-            public string[] contentClasses;
+            public string title;
+            public string[] titleClasses;
 
             public string iconUrl;
             public string[] iconClasses;
+
+            public string contentUrl;
         }
         protected override void Write(UIElementRenderer renderer, JsonFrontMatterBlock frontMatterBlock)
         {
             var json = frontMatterBlock.Lines.ToString();
-            var frontMatter = JsonUtility.FromJson<FrontMatter>(json);
-
-            var header = GetClassedElement<VisualElement>(frontMatter.headerClasses);
-            header.name = nameof(header);
-
-            if (!string.IsNullOrEmpty(frontMatter.iconUrl))
+            try
             {
-                var headerIcon = GetImageElement<Image>(frontMatter.iconUrl, frontMatter.iconClasses);
-                headerIcon.name = nameof(headerIcon);
-                header.Add(headerIcon);
-            }
-            if (!string.IsNullOrEmpty(frontMatter.content))
-            {
-                var title = GetTextElement<Label>(frontMatter.content, frontMatter.contentClasses);
-                title.name = nameof(title);
-                header.Add(title);
-            }
-            else if (IsAssetDirectory(renderer.Document.Data))
-            {
-                var fileName = Path.GetFileNameWithoutExtension(renderer.Document.Data);
-                fileName = ObjectNames.NicifyVariableName(fileName);
-                var title = GetTextElement<Label>(fileName, frontMatter.contentClasses);
-                title.name = nameof(title);
-                header.Add(title);
-            }
+                var frontMatter = JsonUtility.FromJson<FrontMatter>(json);
 
-            renderer.WriteElement(header);
-            var parent = header.parent;
-            if (!parent.HasStyleSheetPath(frontMatter.pageStylePath))
-                parent.AddStyleSheetPath(frontMatter.pageStylePath);
+                var header = GetClassedElement<VisualElement>(frontMatter.headerClasses);
+                header.name = nameof(header);
+
+                if (!string.IsNullOrEmpty(frontMatter.iconUrl))
+                {
+                    var headerIcon = GetImageElement<Image>(frontMatter.iconUrl, frontMatter.iconClasses);
+                    headerIcon.name = nameof(headerIcon);
+                    header.Add(headerIcon);
+                }
+                if (!string.IsNullOrEmpty(frontMatter.title))
+                {
+                    var title = GetTextElement<Label>(frontMatter.title, frontMatter.titleClasses);
+                    title.name = nameof(title);
+                    header.Add(title);
+                }
+                else if (IsAssetDirectory(renderer.Document.Data))
+                {
+                    var fileName = Path.GetFileNameWithoutExtension(renderer.Document.Data);
+                    fileName = ObjectNames.NicifyVariableName(fileName);
+                    var title = GetTextElement<Label>(fileName, frontMatter.titleClasses);
+                    title.name = nameof(title);
+                    header.Add(title);
+                }
+
+                renderer.WriteElement(header);
+
+                if (!string.IsNullOrEmpty(frontMatter.contentUrl))
+                {
+                    var contentElement = GetClassedElement<MarkdownElement>();
+                    contentElement.Data = frontMatter.contentUrl;
+                    renderer.WriteElement(contentElement);
+                    contentElement.RefreshContent();
+                }
+
+                var parent = header.parent;
+                if (!parent.HasStyleSheetPath(frontMatter.pageStylePath))
+                    parent.AddStyleSheetPath(frontMatter.pageStylePath);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                renderer.WriteElement(GetTextElement<Label>(e.Message));
+            }
         }
     }
 }
