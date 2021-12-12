f3c0f2218c0c1f5378011dae638e93ccd8408f93
PassivePicasso <PassivePicasso@users.noreply.github.com>
2021-12-12 01:25:20 -0500

    Update MarkdownFileWatcher to not manage when to notify
    This moves the responsibility of filtering notifications onto the objects which need to be updated.  This allows more flexible implementations and requires less state.
    
    Update Documentation window to update its page listing any time a markdown is added, modified, moved or removed from the project.

--- Editor/Markdown/MarkdownFileWatcher.cs
+++ Editor/Markdown/MarkdownFileWatcher.cs
@@ -1,34 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.IO;
 using UnityEditor;
-using UnityEngine;
 
 namespace ThunderKit.Markdown
 {
     public class MarkdownFileWatcher : AssetPostprocessor
     {
-        static Dictionary<string, HashSet<MarkdownElement>> ActiveElementDocuments = new Dictionary<string, HashSet<MarkdownElement>>();
-        public static void RegisterActiveElement(string document, MarkdownElement element)
-        {
-            if (!ActiveElementDocuments.ContainsKey(document))
-                ActiveElementDocuments[document] = new HashSet<MarkdownElement>();
+        public enum ChangeType { Imported, Deleted, Moved }
 
-            ActiveElementDocuments[document].Add(element);
-        }
-        public static void UnregisterActiveElement(string document, MarkdownElement element)
-        {
-            if (!ActiveElementDocuments.ContainsKey(document)) return;
+        public static event EventHandler<(string path, ChangeType change)> DocumentUpdated;
 
-            ActiveElementDocuments[document].Remove(element);
-        }
         static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
             foreach (string document in importedAssets)
             {
                 if (Path.GetExtension(document) != ".md") continue;
-                if (!ActiveElementDocuments.ContainsKey(document)) continue;
-                foreach (var element in ActiveElementDocuments[document])
-                    element.RefreshContent();
+                DocumentUpdated?.Invoke(null, (document, ChangeType.Imported));
+            }
+            foreach (string document in deletedAssets)
+            {
+                if (Path.GetExtension(document) != ".md") continue;
+                DocumentUpdated?.Invoke(null, (document, ChangeType.Deleted));
             }
         }
     }
