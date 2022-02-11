3a30845c1c02416ed64a96c18328462b319e5cb6
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-02-11 17:46:15 -0500

    Update Settings window

--- Editor/Core/Windows/SettingsWindow.cs
+++ Editor/Core/Windows/SettingsWindow.cs
@@ -5,6 +5,7 @@ using ThunderKit.Core.Windows;
 using UnityEditor;
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 #if UNITY_2019_1_OR_NEWER
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
@@ -18,6 +19,8 @@ namespace ThunderKit.Core.Windows
     public class SettingsWindow : TemplatedWindow
     {
         readonly static string[] searchFolders = new[] { "Assets", "Packages" };
+        private ListView settingsList;
+        private VisualElement settingsArea;
 
         public static event Action OnSettingsLoading;
 
@@ -32,18 +35,36 @@ namespace ThunderKit.Core.Windows
         {
             base.OnEnable();
 
-            var settingsArea = rootVisualElement.Q("settings-area");
-            var settingsPaths = AssetDatabase.FindAssets($"t:{nameof(ThunderKitSetting)}", searchFolders)
-                .Select(AssetDatabase.GUIDToAssetPath).ToArray();
+            settingsList = rootVisualElement.Q<ListView>("settings-list");
+            settingsArea = rootVisualElement.Q("settings-area");
+            var settings = AssetDatabase.FindAssets($"t:{nameof(ThunderKitSetting)}", searchFolders)
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<ThunderKitSetting>)
+                .ToArray();
 
-            foreach (var settingPath in settingsPaths)
+            settingsList.makeItem = OnMakeItem;
+            settingsList.bindItem = OnBindItem; 
+            settingsList.itemsSource = settings;
+            settingsList.selectionType = SelectionType.Multiple;
+
+            settingsList.onSelectionChanged += OnSettingsSelected;
+
+        }
+
+        private void OnBindItem(VisualElement ve, int index)
+        {
+            var label = ve as Label;
+            var setting = settingsList.itemsSource[index] as ThunderKitSetting;
+            label.text = ObjectNames.NicifyVariableName(setting.name);
+        }
+
+        private VisualElement OnMakeItem() => new Label();
+
+        void OnSettingsSelected(IEnumerable<object> obj)
+        {
+            settingsArea.Clear();
+            foreach (var setting in obj.OfType<ThunderKitSetting>())
             {
-                var setting = AssetDatabase.LoadAssetAtPath<ThunderKitSetting>(settingPath);
-                if (!setting)
-                {
-                    AssetDatabase.DeleteAsset(settingPath);
-                    continue;
-                }
                 var settingSection = GetTemplateInstance("ThunderKitSettingSection");
                 var title = settingSection.Q<Label>("title");
                 if (title != null)
@@ -62,5 +83,6 @@ namespace ThunderKit.Core.Windows
                 settingsArea.Add(settingSection);
             }
         }
+
     }
 }
