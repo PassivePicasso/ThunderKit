6e21fdc74ab2d91dbf48ff808b6a31ea5ff08cb1
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-02-27 08:56:07 -0500

    Remove missed ComposableElement error handling

--- Editor/Core/Paths/Components/OutputReference.cs
+++ Editor/Core/Paths/Components/OutputReference.cs
@@ -12,25 +12,16 @@ namespace ThunderKit.Core.Paths.Components
         {
             try
             {
-                Errored = false;
-                ErrorMessage = string.Empty;
                 return reference.GetPath(pipeline);
             }
             catch (NullReferenceException nre)
             {
-                Errored = true;
-                ErrorMessage = nre.Message;
-                ErrorStacktrace = nre.StackTrace;
                 var pathReferencePath = UnityWebRequest.EscapeURL(AssetDatabase.GetAssetPath(output));
                 var pathReferenceLink = $"[{output.name}.{name}.reference](assetlink://{pathReferencePath})";
                 throw new InvalidOperationException($"Error {pathReferenceLink} is unassigned or null", nre);
-
             }
             catch (Exception e)
             {
-                Errored = true;
-                ErrorMessage = e.Message;
-                ErrorStacktrace = e.StackTrace;
                 var pathReferencePath = UnityWebRequest.EscapeURL(AssetDatabase.GetAssetPath(output));
                 var pathReferenceLink = $"[{output.name}.{name}.reference({reference.name})](assetlink://{pathReferencePath})";
                 throw new InvalidOperationException($"Error Invoking PathReference: {pathReferenceLink}", e);
