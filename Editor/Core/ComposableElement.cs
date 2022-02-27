244c6f2441316c21c0a6f51700543bf15bfa26e4
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-02-27 08:34:56 -0500

    Remove outdated error handling on ComposableElement

--- Editor/Core/ComposableElement.cs
+++ Editor/Core/ComposableElement.cs
@@ -4,13 +4,6 @@ namespace ThunderKit.Core
 {
     public class ComposableElement : ScriptableObject
     {
-        [HideInInspector]
-        public bool Errored;
-        [HideInInspector]
-        public string ErrorMessage;
-        [HideInInspector]
-        public string ErrorStacktrace;
-
         private void Awake()
         {
             name = GetType().Name;
