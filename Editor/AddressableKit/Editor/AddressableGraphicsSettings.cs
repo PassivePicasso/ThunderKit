fe15e4e08ffcb4c03401d5666e8fcdc258efaa4a
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-03-09 13:08:52 -0500

    initial commit of addressable editor tools

---
+++ Editor/AddressableKit/Editor/AddressableGraphicsSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Rendering;
+
+namespace AddressablesKit
+{
+    public class AddressableGraphicsSettings
+    {
+        [InitializeOnLoadMethod]
+        static async void Start()
+        {
+            var shader = await Addressables.LoadAssetAsync<Shader>("RoR2/Base/Shaders/Internal-DeferredReflectionsCustom.shader").Task;
+            shader.hideFlags = HideFlags.HideAndDontSave;
+            GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredReflections, shader);
+
+            shader = await Addressables.LoadAssetAsync<Shader>("RoR2/Base/Shaders/Internal-DeferredShadingCustom.shader").Task;
+            shader.hideFlags = HideFlags.HideAndDontSave;
+            GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredShading, shader);
+            GraphicsSettings.SetShaderMode(BuiltinShaderType.DeferredShading, BuiltinShaderMode.UseCustom);
+            GraphicsSettings.SetShaderMode(BuiltinShaderType.DeferredReflections, BuiltinShaderMode.UseCustom);
+            GraphicsSettings.SetShaderMode(BuiltinShaderType.ScreenSpaceShadows, BuiltinShaderMode.UseCustom);
+        }
+    }
+}
