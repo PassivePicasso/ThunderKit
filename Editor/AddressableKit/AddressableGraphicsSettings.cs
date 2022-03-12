ebe9cb136f34aeb4e702bb3d9028af45c49ccedf
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-03-12 16:51:21 -0500

    Clean up and Stabilize GraphicsSettings management
    Fix issue with GraphicsSettings trying to save transient references and throwing errors.
    Merge AddressTrasnformer into AddressableGraphicsSettings

--- Editor/AddressableKit/AddressableGraphicsSettings.cs
+++ Editor/AddressableKit/AddressableGraphicsSettings.cs
@@ -1,26 +1,93 @@
 ﻿#if TK_ADDRESSABLE
+using System.IO;
+using System.Threading.Tasks;
+using ThunderKit.Core.Data;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.Rendering;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.UIElements;
 
 namespace ThunderKit.RemoteAddressables
 {
-    public class AddressableGraphicsSettings
+    public class AddressableGraphicsSettings : ThunderKitSetting
     {
-        [InitializeOnLoadMethod]
-        static async void Start()
-        {
-            var shader = await Addressables.LoadAssetAsync<Shader>("RoR2/Base/Shaders/Internal-DeferredReflectionsCustom.shader").Task;
-            shader.hideFlags = HideFlags.HideAndDontSave;
-            GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredReflections, shader);
+        public string CustomDeferredReflection;
+        public string CustomDeferredShading;
+        public string CustomDeferredScreenspaceShadows;
 
-            shader = await Addressables.LoadAssetAsync<Shader>("RoR2/Base/Shaders/Internal-DeferredShadingCustom.shader").Task;
-            shader.hideFlags = HideFlags.HideAndDontSave;
-            GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredShading, shader);
-            GraphicsSettings.SetShaderMode(BuiltinShaderType.DeferredShading, BuiltinShaderMode.UseCustom);
-            GraphicsSettings.SetShaderMode(BuiltinShaderType.DeferredReflections, BuiltinShaderMode.UseCustom);
-            GraphicsSettings.SetShaderMode(BuiltinShaderType.ScreenSpaceShadows, BuiltinShaderMode.UseCustom);
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        [InitializeOnLoadMethod]
+        public static async Task SetAllShaders()
+        {
+            Addressables.InternalIdTransformFunc = RedirectInternalIdsToGameDirectory;
+            AssetBundle.UnloadAllAssetBundles(true);
+            var initializeOpertaion = Addressables.InitializeAsync();
+            bool wait = true;
+            while (wait && !initializeOpertaion.IsDone)
+            {
+                EditorApplication.QueuePlayerLoopUpdate();
+                await Task.Delay(500);
+            }
+            var settings = GetOrCreateSettings<AddressableGraphicsSettings>();
+            await SetShader(settings.CustomDeferredShading, BuiltinShaderType.DeferredShading);
+            await SetShader(settings.CustomDeferredReflection, BuiltinShaderType.DeferredReflections);
+            await SetShader(settings.CustomDeferredScreenspaceShadows, BuiltinShaderType.ScreenSpaceShadows);
+        }
+
+        public static void UnsetAllShaders()
+        {
+            var settings = GetOrCreateSettings<AddressableGraphicsSettings>();
+             UnsetShader(settings.CustomDeferredShading, BuiltinShaderType.DeferredShading);
+             UnsetShader(settings.CustomDeferredReflection, BuiltinShaderType.DeferredReflections);
+             UnsetShader(settings.CustomDeferredScreenspaceShadows, BuiltinShaderType.ScreenSpaceShadows);
+        }
+
+        static string RedirectInternalIdsToGameDirectory(IResourceLocation location)
+        {
+            var path = location.InternalId.Replace("\\", "/");
+
+            var standardPwd = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "StreamingAssets", "aa").Replace("\\", "/");
+            if (location.ResourceType == typeof(IAssetBundleResource) && path.StartsWith(standardPwd))
+                path = path.Replace(standardPwd, ThunderKitSettings.EditTimePath);
+
+            return path;
+        }
+
+        public static async Task SetShader(string address, BuiltinShaderType shaderType)
+        {
+            if (!string.IsNullOrEmpty(address))
+            {
+                var cdrOp = Addressables.LoadAssetAsync<Shader>(address);
+                bool wait = true;
+                while (wait && !cdrOp.IsDone)
+                {
+                    EditorApplication.QueuePlayerLoopUpdate();
+                    await Task.Delay(500);
+                }
+                var cdr = cdrOp.Result;
+                cdr.hideFlags = HideFlags.HideAndDontSave;
+                GraphicsSettings.SetCustomShader(shaderType, cdr);
+                GraphicsSettings.SetShaderMode(shaderType, BuiltinShaderMode.UseCustom);
+            }
+            else GraphicsSettings.SetShaderMode(shaderType, BuiltinShaderMode.UseBuiltin);
+        }
+
+        public static void UnsetShader(string address, BuiltinShaderType shaderType)
+        {
+            if (!string.IsNullOrEmpty(address))
+            {
+                GraphicsSettings.SetCustomShader(shaderType, null);
+                GraphicsSettings.SetShaderMode(shaderType, BuiltinShaderMode.UseBuiltin);
+            }
+        }
+
+        public override void CreateSettingsUI(VisualElement rootElement)
+        {
+            base.CreateSettingsUI(rootElement);
+            rootElement.Add(new Button(async () => await SetAllShaders()) { text = "Reload" });
         }
     }
 }
---
+++ Editor/AddressableKit/GraphicsSettingsPreProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace ThunderKit.RemoteAddressables
+{
+    public class GraphicsSettingsPreProcessor : AssetModificationProcessor
+    {
+        private const string GraphicsSettingsPath = "ProjectSettings/GraphicsSettings.asset";
+
+        public static string[] OnWillSaveAssets(string[] paths)
+        {
+            if (paths.Contains(GraphicsSettingsPath))
+                AddressableGraphicsSettings.UnsetAllShaders();
+
+            return paths;
+        }
+    }
+}
