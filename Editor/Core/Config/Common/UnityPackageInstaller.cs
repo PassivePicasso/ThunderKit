a7f078744e25678de670b5af90c3fcb1c368627a
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-04-23 18:33:12 -0400

    Add abstract UPM Installer and Uninstaller base classes

---
+++ Editor/Core/Config/Common/UnityPackageInstaller.cs
@@ -0,0 +1,25 @@
+﻿using ThunderKit.Core.Utilities;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+
+namespace ThunderKit.Core.Config.Common
+{
+    public abstract class UnityPackageInstaller : OptionalExecutor
+    {
+        public abstract string PackageIdentifier { get; }
+
+        public sealed override void Execute()
+        {
+            Request result = Client.Add(PackageIdentifier);
+            var escape = false;
+            while (!result.IsCompleted && !escape)
+            {
+                var x = escape; //Break and set in case of unexpected infinite loop
+            }
+            Debug.Log($"Installed {PackageIdentifier}");
+
+            PackageHelper.ResolvePackages();
+        }
+    }
+}
