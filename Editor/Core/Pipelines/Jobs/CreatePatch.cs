1d77fe93d52eec07e99c056dcd197101b81244a3
PassivePicasso <PassivePicasso@users.noreply.github.com>
2022-04-27 21:10:49 -0400

    add tooltips to create patch

--- Editor/Core/Pipelines/Jobs/CreatePatch.cs
+++ Editor/Core/Pipelines/Jobs/CreatePatch.cs
@@ -1,12 +1,16 @@
 ﻿using System.Threading.Tasks;
+using UnityEngine;
 
 namespace ThunderKit.Core.Pipelines.Jobs
 {
     [PipelineSupport(typeof(Pipeline))]
     public class CreatePatch : PipelineJob
     {
+        [Tooltip("Assembly that the patch will be applied to")]
         public string OldFile;
+        [Tooltip("Assembly that the patch will change Old File into")]
         public string NewFile;
+        [Tooltip("Output Patch file that will be used to convert OldFile into NewFile")]
         public string PatchFile;
 
         public override Task Execute(Pipeline pipeline)
