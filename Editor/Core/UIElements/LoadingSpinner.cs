35b209c0f032eea1c3a4d4c1d6e3dd862770c51e
PassivePicasso <PassivePicasso@users.noreply.github.com>
2023-03-06 15:54:27 -0500

    Fix LoadingSpinner usings for multi-version differences

--- Editor/Core/UIElements/LoadingSpinner.cs
+++ Editor/Core/UIElements/LoadingSpinner.cs
@@ -1,6 +1,10 @@
 using UnityEditor;
 using UnityEngine;
+#if UNITY_2019_1_OR_NEWER
 using UnityEngine.UIElements;
+#else
+using UnityEngine.Experimental.UIElements;
+#endif
 
 namespace ThunderKit.Core.UIElements
 {
