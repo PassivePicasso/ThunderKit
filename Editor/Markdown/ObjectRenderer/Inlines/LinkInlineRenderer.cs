23b80b89d677925c8cbc26b271ef822cdd685654
PassivePicasso <PassivePicasso@users.noreply.github.com>
2023-03-11 20:56:33 -0500

    Update Copy, Code + Documentation

--- Editor/Markdown/ObjectRenderer/Inlines/LinkInlineRenderer.cs
+++ Editor/Markdown/ObjectRenderer/Inlines/LinkInlineRenderer.cs
@@ -64,6 +64,8 @@ namespace ThunderKit.Markdown.ObjectRenderers
 
                 return container;
             });
+            RegisterScheme("file", link => System.Diagnostics.Process.Start(link));
+
             RegisterScheme("menulink", link =>
             {
                 var schemelessUri = link.Substring("menulink://".Length);
