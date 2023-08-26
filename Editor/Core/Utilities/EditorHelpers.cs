9df4397014fceb7c50cc0e738a0850319bbaa7f2
PassivePicasso <PassivePicasso@users.noreply.github.com>
2023-08-26 14:03:40 -0400

    Remove unnecessary code
    Removes the fix replaced with the PathReferenceResolverDrawer

--- Editor/Core/Utilities/EditorHelpers.cs
+++ Editor/Core/Utilities/EditorHelpers.cs
@@ -33,34 +33,5 @@ namespace ThunderKit.Core.Utilities
                 property.serializedObject.ApplyModifiedProperties();
             }
         }
-        public static void DrawSanitizedPropertiesExcluding(SerializedObject obj,  params string[] propertyToExclude)
-        {
-            SerializedProperty iterator = obj.GetIterator();
-            bool enterChildren = true;
-            while (iterator.NextVisible(enterChildren))
-            {
-                enterChildren = false;
-                if (!propertyToExclude.Contains(iterator.name))
-                {
-                    EditorGUI.BeginChangeCheck();
-                    EditorGUILayout.PropertyField(iterator, true);
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        if (iterator.propertyType == SerializedPropertyType.String)
-                        {
-                            iterator.stringValue = new string(iterator.stringValue.Where(x => !char.IsControl(x)).ToArray());
-                        }
-                        else if (iterator.isArray && iterator.arrayElementType == "string")
-                        {
-                            for (int i = 0; i < iterator.arraySize; i++)
-                            {
-                                SerializedProperty prop = iterator.GetArrayElementAtIndex(i);
-                                prop.stringValue = new string(prop.stringValue.Where(x => !char.IsControl(x)).ToArray());
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 }
