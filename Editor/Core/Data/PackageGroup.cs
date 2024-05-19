7d620eb17e461cf9416ed7a8a0df8a7be341ca29
PassivePicasso <PassivePicasso@users.noreply.github.com>
2024-05-19 13:56:18 -0400

    Potential fix for hangs from package source loading

--- Editor/Core/Data/PackageGroup.cs
+++ Editor/Core/Data/PackageGroup.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 namespace ThunderKit.Core.Data
 {
     [Serializable]
-    public class PackageGroup : ScriptableObject, IEquatable<PackageGroup>
+    public class PackageGroup : IEquatable<PackageGroup>
     {
         public PackageVersion this[string version]
         {
