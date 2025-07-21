bf3cd70b98ce035fbc9a5248f13bd3236ef56e21
VetleNeumann <vetle_er_her@hotmail.com>
2025-07-21 19:34:37 +0200

    Fix tests for Unity 2018

--- Tests/Editor/PackageSourceSettingsTests.cs
+++ Tests/Editor/PackageSourceSettingsTests.cs
@@ -1,14 +1,43 @@
 namespace ThunderKitTests
 {
+    using Boo.Lang.Runtime;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
     using System.Configuration;
     using System.Linq;
+    using System.Reflection;
     using ThunderKit.Core.Data;
     using UnityEditor;
     using UnityEngine;
+#if UNITY_2019_1_OR_NEWER
     using UnityEngine.UIElements;
+    using UnityEditor.UIElements;
+#else
+    using UnityEngine.Experimental.UIElements;
+#endif
+
+    [TestFixture]
+    public class SanityTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            Debug.Log("testing");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Debug.Log("cleaning up");
+        }
+
+        [Test]
+        public void SanityTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
 
     [TestFixture]
     public class PackageSourceSettingsTests
@@ -22,20 +51,27 @@ namespace ThunderKitTests
         [SetUp]
         public void Init()
         {
-            initialPackageSources = PackageSourceSettings.PackageSources.ToList();
-
-            root = new VisualElement();
             packageSourceSettings = ScriptableObject.CreateInstance<PackageSourceSettings>();
+            root = new VisualElement();
             packageSourceSettings.CreateSettingsUI(root);
             sourcesList = root.Q<ListView>("sources-list");
 
+            Utility_EditorApplicationUpdate();
+            initialPackageSources = PackageSourceSettings.PackageSources.ToList();
             numSourcesInitially = sourcesList.itemsSource.Count;
+        }
+
+        [Test]
+        public void SanityTest()
+        {
+            Assert.AreEqual(1, 1);
         }
 
         [TearDown]
         public void Cleanup()
         {
             // Delete all sources that were added during the test
+            Utility_EditorApplicationUpdate();
             var introducedSources = PackageSourceSettings.PackageSources
                                         .Except(initialPackageSources)
                                         .ToList();
@@ -43,8 +79,23 @@ namespace ThunderKitTests
             {
                 PackageSourceSettings.RemoveSource(source);
             }
-            EditorApplication.update();
+            Utility_EditorApplicationUpdate();
             Assert.That(PackageSourceSettings.PackageSources, Is.EqualTo(initialPackageSources));
+        }
+
+        private void Utility_EditorApplicationUpdate()
+        {
+#if UNITY_2019_1_OR_NEWER
+            EditorApplication.update();
+#else
+            // In Unity 2018 and prior, EditorApplication is paused during tests,
+            // so EditorApplication.update() does not run. Invoke methods directly to refresh the package sources.
+            var type = typeof(PackageSourceSettings);
+            var methodDeferredRefresh = type.GetMethod("DeferredRefresh", BindingFlags.NonPublic | BindingFlags.Static);
+            var methodRefreshList = type.GetMethod("RefreshList", BindingFlags.NonPublic | BindingFlags.Instance);
+            methodDeferredRefresh.Invoke(null, null);
+            methodRefreshList.Invoke(packageSourceSettings, null);
+#endif
         }
 
         private PackageSource Utility_AddThunderStoreSource()
@@ -55,7 +106,7 @@ namespace ThunderKitTests
             Type[] packageSourceTypes = PackageSourceSettings.GetAvailablePackageSourceTypes();
             var thunderstoreSourceType = packageSourceTypes.First(x => x.Name == "ThunderstoreSource");
             packageSourceSettings.AddSource(thunderstoreSourceType);
-            EditorApplication.update();
+            Utility_EditorApplicationUpdate();
 
             return PackageSourceSettings.PackageSources
                 .Except(priorPackageSources)
@@ -78,7 +129,7 @@ namespace ThunderKitTests
             // Select the added source, and press remove
             sourcesList.selectedIndex = sourcesList.itemsSource.IndexOf(addedSource);
             packageSourceSettings.RemoveSourceClicked();
-            EditorApplication.update();
+            Utility_EditorApplicationUpdate();
 
             // List of sources should be same as initially
             Assert.That(sourcesList.itemsSource.Count, Is.EqualTo(numSourcesInitially));
