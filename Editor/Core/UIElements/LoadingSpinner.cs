6eb2fc4864fcaf9a28435d540c7326a0dad1bed4
VetleNeumann <vetle_er_her@hotmail.com>
2025-07-23 17:04:05 +0200

    Fix package manager loading spinner for Unity 2018

--- Editor/Core/UIElements/LoadingSpinner.cs
+++ Editor/Core/UIElements/LoadingSpinner.cs
@@ -25,12 +25,20 @@ namespace ThunderKit.Core.UIElements
             innerElement = new VisualElement();
             innerElement.AddToClassList("loading-spinner");
             innerElement.AddToClassList("hidden");
+            SetLoadingSpinnerBackground();
+
             Add(innerElement);
         }
 
         private void UpdateProgress()
         {
-            innerElement.transform.rotation = Quaternion.Euler(0, 0, m_Rotation);
+            var rotation = Quaternion.Euler(0, 0, m_Rotation);
+#if UNITY_2019_1_OR_NEWER
+            innerElement.transform.rotation = rotation;
+#else
+            this.transform.rotation = rotation;
+#endif
+
             m_Rotation += 3;
             if (m_Rotation > 360)
                 m_Rotation -= 360;
@@ -58,6 +66,19 @@ namespace ThunderKit.Core.UIElements
 
             started = false;
             innerElement.AddToClassList("hidden");
+        }
+
+        private void SetLoadingSpinnerBackground()
+        {
+            // Unity 2019.1+ includes this as a loaded resource, but in earlier versions
+            // we need to load it from the Unity Package Manager package's resources.
+#if UNITY_2019_1_OR_NEWER
+            innerElement.style.backgroundImage = new StyleBackground(
+                (Texture2D)EditorGUIUtility.Load("icons/packagemanager/dark/loading.png")
+            );
+#else
+            innerElement.style.backgroundImage = AssetDatabase.LoadAssetAtPath<Texture2D>("Packages/com.unity.package-manager-ui/Editor/Resources/Images/Dark/loading.png");
+#endif
         }
     }
 }
