dadbe3c8ea383856e3f9b08faeb08422b9b85b25
Zantox <vetle_er_her@hotmail.com>
2025-07-26 16:15:48 +0200

    Fix Package Manager loading spinner (#112)
    * Fix Package Manager loading wheel spinning around wrong center
    
    * Fix package manager loading spinner for Unity 2018
    
    * Make LoadingSpinner rotation uniform accross versions

--- Editor/Core/UIElements/LoadingSpinner.cs
+++ Editor/Core/UIElements/LoadingSpinner.cs
@@ -25,12 +25,22 @@ namespace ThunderKit.Core.UIElements
             innerElement = new VisualElement();
             innerElement.AddToClassList("loading-spinner");
             innerElement.AddToClassList("hidden");
+#if UNITY_2021_2_OR_NEWER
+            // In Unity 2021.2 and later, transform origin of elements changed from
+            // top-left to center by default.
+            // https://discussions.unity.com/t/introducing-transform-styles/861646
+            // To maintain the same behavior as before, we set the transform origin to top-left.
+            style.transformOrigin = new TransformOrigin(Length.Percent(0), Length.Percent(0), 0);
+#endif
+            SetLoadingSpinnerBackground();
+
             Add(innerElement);
         }
 
         private void UpdateProgress()
         {
             transform.rotation = Quaternion.Euler(0, 0, m_Rotation);
+
             m_Rotation += 3;
             if (m_Rotation > 360)
                 m_Rotation -= 360;
@@ -58,6 +68,19 @@ namespace ThunderKit.Core.UIElements
 
             started = false;
             innerElement.AddToClassList("hidden");
+        }
+
+        private void SetLoadingSpinnerBackground()
+        {
+            // Unity 2019.1+ includes this as a loaded resource, but in earlier versions
+            // we need to load it from the Unity Package Manager package's resources.
+#if UNITY_2019_1_OR_NEWER
+            innerElement.style.backgroundImage = new StyleBackground(
+                (Texture2D)EditorGUIUtility.Load("icons/packagemanager/dark/loading.png")
+            );
+#else
+            innerElement.style.backgroundImage = AssetDatabase.LoadAssetAtPath<Texture2D>("Packages/com.unity.package-manager-ui/Editor/Resources/Images/Dark/loading.png");
+#endif
         }
     }
 }
